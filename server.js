1571b16e80ccba6d2b9dd256a50310123e6eb35f
johnoppenheimer <maximecattet@gmx.com>
2016-01-08 17:30:45 +0100

    remove useless files

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-var pmx = require('pmx');
-pmx.init();
-
 var express = require('express');
 var app = express();
 var request = require('request');
@@ -10,43 +7,33 @@ app.use('/startup', express.static(__dirname + '/startup'));
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/video', express.static(__dirname + '/video'));
 
-app.get('/homepage', function(req, res){
-	res.render('index');
+app.get('/', function(req, res){
+
 });
 
-app.get('/:id', function(req, res){
-	console.log('Media requested ' + req.params.id);
-	var options = {
-		uri: "https://krakmobileservice.azure-mobile.net/api/MediaShare/" + req.params.id,
-		method: "GET"
-	};
-	request(options,
-		function(error, response, body){
-			var json = JSON.parse(body);
-			res.render('shot', {
-				result: json
-			});
-		}
-	);
+app.get('/app', function(req, res){
+
 });
 
-app.get('/bestspotLA', function(req, res){
-	res.render('bestspotLA');
+app.get('/map', function(req, res){
+
 });
 
-app.get('/bestspotbarcelona', function(req, res){
-	res.render('bestspotbarcelona');
+app.get('/box', function(req, res){
+
+})
+
+app.get('/team', function(req, res){
+
 });
 
 app.get('/privacy-policy', function(req, res){
-	res.render('privacy-policy');
+	res.render('views/privacy-policy');
 });
 
 app.get('*', function(req, res){
 	res.redirect('/');
 });
 
-app.listen(80);
-console.log('80 is the magic port');
-
-app.use(pmx.expressErrorHandler());
+app.listen(8080);
+console.log('8080 is the magic port');
