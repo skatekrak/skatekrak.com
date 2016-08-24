e79ac8b7ab927d5b820ff1679d3e69c18b4d73c8
Maxime <maximecattet@gmx.com>
2016-08-24 14:27:59 +0200

    cleaning

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var app = express();
-var ECT = require('ect');
-var request = require('request');
 
-var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
 app.set('view engine', 'ect');
 app.engine('ect', ectRenderer.render);
 app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/video', express.static(__dirname + '/video'));
 
 app.get('/', function(req, res){
 	res.render('index');
@@ -22,5 +17,5 @@ app.get('*', function(req, res){
 	res.redirect('/');
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log('8080 is the magic port');
