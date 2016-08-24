aa2eefac9639f62e409eab71a2ad6994c5ce95c2
Maxime <maximecattet@gmx.com>
2016-08-24 14:41:04 +0200

    add index

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ect');
-app.engine('ect', ectRenderer.render);
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.sendFile(__dirname + '/views/index.html')
 });
 
 app.get('/privacy-policy', function(req, res){
