4054cfddf96b1e19da91700b20675656dcbab65c
Maxime Cattet <maximecattet@gmx.com>
2018-10-20 17:46:23 +0100

    :sparkles: Add subscribe function

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+var $ = require('jquery');
 /* ---- ELEMENTS ---- */
 
 /* header top */
@@ -10,4 +11,23 @@ burgerMenu.addEventListener('click', function(event) {
     menuContainer.classList.toggle('show-header');
     burgerMenu.classList.toggle('burger-open');
     header.classList.toggle('header-open');
+});
+
+$('#main-top-newsletter').submit(function(event) {
+    event.preventDefault();
+    const email = $("#main-top-newsletter-input").val();
+    $.ajax({
+        url: "https://barde.skatekrak.com/emails",
+        type: "POST",
+        data: { email },
+        dataType: "json",
+        success: function(data) {
+            // TODO
+            console.log(data);
+        },
+        error: function(e) {
+            // TODO
+            console.error(e);
+        }
+    });
 });
