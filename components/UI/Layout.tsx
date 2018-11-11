1f20a2c51c5550caad255bf4a4beef72fef49222
Arthur <a@skatekrak.com>
2018-11-11 13:19:02 +0000

    :bug: Fix import styl files order in Layout.tsx

--- components/UI/Layout.tsx
+++ components/UI/Layout.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 
 import Header from '../Header';
 
+import '../../static/styles/reset.css';
+import '../../static/styles/stylus-mq.styl';
+import '../../static/styles/styleguide.styl';
 import '../../static/styles/fonts.styl';
 import '../../static/styles/helpers.styl';
 import '../../static/styles/main.styl';
-import '../../static/styles/reset.css';
-import '../../static/styles/styleguide.styl';
-import '../../static/styles/stylus-mq.styl';
 
 type Props = {
     head?: Head;
