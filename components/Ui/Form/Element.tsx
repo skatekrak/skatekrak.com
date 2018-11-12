becd5fb6a1a3ec46262cdcc78660ad3993a54274
Arthur <a@skatekrak.com>
2018-11-12 14:59:36 +0000

    :sparkles: Create FormElement component

---
+++ components/Ui/Form/Element.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import 'static/styles/form.styl';
+
+type Props = {
+    label: string;
+    children: React.ReactElement<React.ReactChildren>;
+};
+
+type State = {
+    errorMessage?: string;
+};
+
+export default class Element extends React.Component<Props, State> {
+    state = {
+        errorMessage: null,
+    };
+    public render() {
+        const { label, children } = this.props;
+        const elementId = `id-${children.props.name}`;
+
+        return (
+            <div className="form-element">
+                <div className="form-element-label">
+                    <label htmlFor={elementId}>{label}</label>
+                </div>
+                <div className="form-element-field">{children}</div>
+            </div>
+        );
+    }
+}
