9d9c37dcc25f2528c81b96759fb395606f2e83a4
Maxime Cattet <maximecattet@gmx.com>
2018-11-13 14:53:47 +0000

    :recycle: Moving shipping form away

--- pages/club.tsx
+++ pages/club.tsx
@@ -1,14 +1,10 @@
-import axios from 'axios';
-import classNames from 'classnames';
-import validator from 'email-validator';
 import * as React from 'react';
-import { Field, InjectedFormProps, reduxForm, SubmissionError } from 'redux-form';
 
 import Layout from 'components/Layout/Layout';
 import 'static/styles/checkout.styl';
 
-import Address from 'components/Ui/Form/Address';
-import RenderInput from 'components/Ui/Form/Input';
+import PaymentForm from 'components/pages/club/PaymentForm';
+import ShippingForm from 'components/pages/club/ShippingForm';
 
 const countriesOptions = [
     {
@@ -65,55 +61,17 @@ const countriesOptions = [
     },
 ];
 
-const validate = (values) => {
-    const errors: any = {};
-
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!validator.validate(values.email)) {
-        errors.email = 'Email not valid';
-    }
-
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    }
-
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    }
-
-    if (!values.line1) {
-        errors.line1 = 'Required';
-    }
-
-    if (!values.city) {
-        errors.city = 'Required';
-    }
-
-    if (!values.postalcode) {
-        errors.postalcode = 'Required';
-    }
-
-    if (!values.country) {
-        errors.country = 'Required';
-    }
-
-    return errors;
-};
-
 type State = {
     step: 'shipping' | 'payment' | 'done';
 };
 
-class Club extends React.Component<InjectedFormProps, State> {
+class Club extends React.Component<{}, State> {
     public state: State = {
-        step: 'shipping',
+        step: 'payment',
     };
 
     public render() {
-        const { handleSubmit, valid, submitting } = this.props;
         const { step } = this.state;
-        const disabledButton = !valid || submitting;
         return (
             <Layout>
                 <div id="checkout" className="container">
@@ -126,82 +84,17 @@ class Club extends React.Component<InjectedFormProps, State> {
                         </div>
                     </header>
                     <main id="checkout-main">
-                        {step === 'shipping' && (
-                            <form
-                                id="checkout-form-shipping"
-                                className="checkout-form"
-                                onSubmit={handleSubmit(this.doSomethingAfterFormIsValid)}
-                            >
-                                <div className="row">
-                                    <div className="form-section col-xs-12 col-md-6">
-                                        <p className="form-section-title">Shipping information</p>
-                                        <p className="form-section-description">
-                                            Give us your shipping information so we can send you the best skateboard
-                                            right on your doorstep!
-                                        </p>
-                                        <div className="checkout-form-fields-container">
-                                            <Field
-                                                name="email"
-                                                component={RenderInput}
-                                                type="email"
-                                                label="Email address"
-                                            />
-                                            <Address countries={countriesOptions} />
-                                        </div>
-                                    </div>
-                                    <div className="form-section col-xs-12 col-md-offset-1 col-md-5">
-                                        <button
-                                            className={classNames('checkout-form-submit-button', {
-                                                'checkout-form-submit-button--enable': !disabledButton,
-                                            })}
-                                            disabled={disabledButton}
-                                        >
-                                            Payment
-                                        </button>
-                                        <div id="checkout-contact">
-                                            <h4 id="checkout-contact-title">Questions?</h4>
-                                            <p>
-                                                Contact our support team at{' '}
-                                                <a id="checkout-contact-mail" href="mailto:club@skatekrak.com">
-                                                    club@skatekrak.com
-                                                </a>
-                                            </p>
-                                        </div>
-                                        <img
-                                            className="checkout-form-img"
-                                            src="static/images/step_1_2x.png"
-                                            alt="Kraken illustration step 1"
-                                        />
-                                    </div>
-                                </div>
-                            </form>
-                        )}
+                        {step === 'shipping' && <ShippingForm onSubmit={this.nextPage} countries={countriesOptions} />}
+                        {step === 'payment' && <PaymentForm onSubmit={this.nextPage} />}
                     </main>
                 </div>
             </Layout>
         );
     }
 
-    private doSomethingAfterFormIsValid = (values) => {
-        const { email } = values;
-        return axios
-            .get('http://localhost:3737/customers/find', {
-                params: {
-                    email,
-                },
-            })
-            .then(() => this.setState({ step: 'payment' }))
-            .catch(() => {
-                throw new SubmissionError({
-                    email: 'Email is alreay taken',
-                    _error: 'Failed',
-                });
-            });
+    private nextPage = () => {
+        this.setState({ step: 'payment' });
     };
 }
 
-export default reduxForm({
-    form: 'shipping',
-    destroyOnUnmount: false,
-    validate,
-})(Club);
+export default Club;
