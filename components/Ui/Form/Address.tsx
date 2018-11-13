75bb41833e55c9f704e31c648ea6326b0f9505b7
Maxime Cattet <maximecattet@gmx.com>
2018-11-13 08:38:42 +0000

    :recycle: Refactor Address component as stateless

--- components/Ui/Form/Address.tsx
+++ components/Ui/Form/Address.tsx
@@ -5,32 +5,22 @@ import RenderSelect from 'components/Ui/Form/Select';
 import { Field } from 'redux-form';
 
 type Props = {
-    address?: object;
     countries: {
         value: string;
         label: string;
     }[];
 };
 
-type State = {
-    address: object;
-};
+const Element: React.SFC<Props> = ({ countries }: Props) => (
+    <React.Fragment>
+        <Field label="First name" name="firstName" component={RenderInput} type="text" />
+        <Field label="Last name" name="lastName" component={RenderInput} type="text" />
+        <Field label="Address" name="line1" component={RenderInput} type="text" />
+        <Field label="Apt/unit etc (optional)" name="line2" component={RenderInput} type="text" />
+        <Field label="City" name="city" component={RenderInput} type="text" />
+        <Field label="Postal code" name="postalcode" component={RenderInput} type="text" />
+        <Field label="Country" name="country" component={RenderSelect} options={countries} />
+    </React.Fragment>
+);
 
-export default class Element extends React.Component<Props, State> {
-    public state: State = {
-        address: {},
-    };
-    public render() {
-        return (
-            <React.Fragment>
-                <Field label="First name" name="firstName" component={RenderInput} type="text" required />
-                <Field label="Last name" name="lastName" component={RenderInput} type="text" required />
-                <Field label="Address" name="line1" component={RenderInput} type="text" required />
-                <Field label="Apt/unit etc (optional)" name="line2" component={RenderInput} type="text" />
-                <Field label="City" name="city" component={RenderInput} type="text" required />
-                <Field label="Postal code" name="postalcode" component={RenderInput} type="text" required />
-                <Field label="Country" name="country" component={RenderSelect} options={this.props.countries} />
-            </React.Fragment>
-        );
-    }
-}
+export default Element;
