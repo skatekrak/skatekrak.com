fcaa1351c7e3970602ba2f715ddad15fcdbed9ef
Maxime Cattet <maximecattet@gmx.com>
2018-11-13 14:52:43 +0000

    :wrench: Add dotenv

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ const withTypescript = require('@zeit/next-typescript');
 const withStylus = require('@zeit/next-stylus');
 const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
-module.exports = withPlugins([withTypescript, withCSS, withStylus]);
+const webpack = require('webpack');
+const { parsed: localEnv } = require('dotenv').config();
+
+module.exports = withPlugins([withTypescript, withCSS, withStylus], {
+    webpack: (config, options) => {
+        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+        return config;
+    },
+});
