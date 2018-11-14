683fcdc01d17f4f402121b912f85cf37a1ec185e
Arthur <a@skatekrak.com>
2018-11-14 10:51:13 +0000

    :sparkles: Add ErrorMessage to FormElement

--- components/Ui/Form/render.tsx
+++ components/Ui/Form/render.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import FormElement from 'components/Ui/Form/Element';
+import ErrorMessage from 'components/Ui/Form/ErrorMessage';
 
 export const createRenderer = (render) => ({ input, meta, label, ...rest }) => (
-    <React.Fragment>
-        <FormElement label={label}>{render(input, rest)}</FormElement>
-        {meta.error && meta.touched && !meta.active && <span>{meta.error}</span>}
-    </React.Fragment>
+    <div className="form-element">
+        <FormElement invalid={meta.error && meta.touched && !meta.active} label={label}>
+            {render(input, rest)}
+        </FormElement>
+        {meta.error && meta.touched && !meta.active && <ErrorMessage message={meta.error} />}
+    </div>
 );
