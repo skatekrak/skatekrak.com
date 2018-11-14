548e2371c0fa7315e95c4e1b34d0077df3c865fd
Maxime Cattet <maximecattet@gmx.com>
2018-11-14 11:22:03 +0000

    :sparkles: Customize price based on country in the query

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,16 @@ import withReduxStore from '../hocs/withRedux';
 import { Provider } from 'react-redux';
 
 class MyApp extends App {
+    static async getInitialProps({ Component, router, ctx }) {
+        let pageProps = {};
+
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return { pageProps };
+    }
+
     render() {
         const { Component, pageProps, reduxStore } = this.props;
         return (
