34540e48eddd747839d0bda06ee61fc901b3a828
Maxime Cattet <maximecattet@gmx.com>
2018-11-15 19:06:11 +0000

    :sparkles: Add query to custom link

--- components/Link.js
+++ components/Link.js
@@ -2,7 +2,7 @@ import { withRouter } from 'next/router';
 import Link from 'next/link';
 import React, { Children } from 'react';
 
-const ActiveLink = ({ router, children, ...props }) => {
+const ActiveLink = ({ router, children, href, ...props }) => {
     const child = Children.only(children);
 
     let className = child.props.className || '';
@@ -12,7 +12,22 @@ const ActiveLink = ({ router, children, ...props }) => {
 
     delete props.activeClassName;
 
-    return <Link {...props}>{React.cloneElement(child, { className })}</Link>;
+    return (
+        <Link href={`${href}${encodeQueryData(router.query)}`} {...props}>
+            {React.cloneElement(child, { className })}
+        </Link>
+    );
 };
+
+function encodeQueryData(data) {
+    const ret = [];
+    for (let d in data) ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+    let str = '';
+    if (ret.length > 0) {
+        const join = ret.join('&');
+        str = `/?${join}`;
+    }
+    return str;
+}
 
 export default withRouter(ActiveLink);
