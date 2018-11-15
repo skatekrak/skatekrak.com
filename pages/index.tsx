1f0b636eababbacec66a2c58e43ef69ac8ca0569
Maxime Cattet <maximecattet@gmx.com>
2018-11-15 10:12:44 +0000

    :sparkles: Add email subscribe to newsletter form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,82 @@
-import * as React from 'react';
+import axios, { AxiosError } from 'axios';
+import validator from 'email-validator';
+import React from 'react';
 
 import Layout from 'components/Layout/Layout';
 import Page from 'components/pages/Page';
+import ErrorMessage from 'components/Ui/Form/ErrorMessage';
+import LikeIcon from 'components/Ui/Icons/Like';
+
+type State = {
+    subscribed: boolean;
+    subscribeError?: string;
+    email: string;
+};
 
 /* tslint:disable:max-line-length */
-const Index: React.SFC<{}> = () => (
-    <Page>
-        <Layout>
-            <React.Fragment>
-                <div id="home-container">
-                    <form id="home-newsletter">
-                        <h2 id="home-newsletter-title">Get hooked up :</h2>
-                        <input type="mail" id="home-newsletter-input" placeholder="love@skatekrak.com" />
-                        <button id="home-newsletter-button">
-                            Subscribe
-                            <svg id="home-newsletter-button-icon" viewBox="0 0 48 48">
-                                <title>Like</title>
-                                <g className="icon-like">
-                                    <path
-                                        className="icon-like-stroke"
-                                        d="M23.86,46,4.8,27.06A12.38,12.38,0,0,1,4.73,9.57a12.42,12.42,0,0,1,17.54.27l1.6,1.6L25.7,9.63A12.29,12.29,0,0,1,34.45,6h0a12.31,12.31,0,0,1,8.76,3.64,12.19,12.19,0,0,1-.29,17.42ZM9.48,28.67,23.86,43,41.4,25.53a10.06,10.06,0,0,0,.29-14.37,10.18,10.18,0,0,0-7.23-3h0a10.16,10.16,0,0,0-7.23,3ZM13.26,8.23a9.84,9.84,0,0,0-7,2.87,10.22,10.22,0,0,0,.07,14.44L8,27.15,22.34,13l-1.59-1.59A10.61,10.61,0,0,0,13.26,8.23Z"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware"
-                                        cx="12.34"
-                                        cy="19.9"
-                                        r="1.04"
-                                        transform="translate(-10.46 14.55) rotate(-45)"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware-2"
-                                        data-name="like-hardware"
-                                        cx="15.26"
-                                        cy="16.97"
-                                        r="1.04"
-                                        transform="translate(-7.53 15.76) rotate(-45)"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware-3"
-                                        data-name="like-hardware"
-                                        cx="32.5"
-                                        cy="16.97"
-                                        r="1.04"
-                                        transform="translate(-2.48 27.95) rotate(-45)"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware-4"
-                                        data-name="like-hardware"
-                                        cx="29.53"
-                                        cy="19.94"
-                                        r="1.04"
-                                        transform="translate(-5.45 26.72) rotate(-45)"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware-5"
-                                        data-name="like-hardware"
-                                        cx="35.43"
-                                        cy="19.9"
-                                        r="1.04"
-                                        transform="translate(-3.69 30.88) rotate(-45)"
-                                    />
-                                    <circle
-                                        className="icon-like-hardware-6"
-                                        data-name="like-hardware"
-                                        cx="32.45"
-                                        cy="22.87"
-                                        r="1.04"
-                                        transform="translate(-6.67 29.65) rotate(-45)"
-                                    />
-                                </g>
-                            </svg>
-                        </button>
-                    </form>
-                    <div id="home-image" />
-                </div>
-                <div id="quote-container">
-                    <p
-                        id="quote"
-                        data-text="Skateboarding is not a hobby. And it's not a sport. Skateboarding is way of learning how to
-                redefine the world around you. Ian Mackaye&nbsp;-&nbsp;"
-                    >
-                        Skateboarding is not a hobby. And it's not a sport. Skateboarding is way of learning how to
-                        redefine the world around you. Ian Mackaye&nbsp;-&nbsp;
-                    </p>
-                </div>
-            </React.Fragment>
-        </Layout>
-    </Page>
-);
+class Index extends React.PureComponent<{}, State> {
+    public state: State = {
+        email: '',
+        subscribed: false,
+        subscribeError: null,
+    };
+
+    public render() {
+        const { email, subscribed, subscribeError } = this.state;
+        return (
+            <Page>
+                <Layout>
+                    <React.Fragment>
+                        <div id="home-container">
+                            <form id="home-newsletter" onSubmit={this.handleSubmit}>
+                                <h2 id="home-newsletter-title">Get hooked up :</h2>
+                                <input
+                                    type="mail"
+                                    id="home-newsletter-input"
+                                    placeholder="love@skatekrak.com"
+                                    value={email}
+                                    onChange={this.emailOnChange}
+                                />
+                                <button id="home-newsletter-button">
+                                    Subscribe
+                                    <LikeIcon />
+                                </button>
+                                {subscribeError && <ErrorMessage message={subscribeError} />}
+                                {subscribed && <span>We sent you an email to confirm</span>}
+                            </form>
+                            <div id="home-image" />
+                        </div>
+                        <div id="quote-container">
+                            <p
+                                id="quote"
+                                data-text="Skateboarding is not a hobby. And it's not a sport. Skateboarding is way of learning how to redefine the world around you. Ian Mackaye&nbsp;-&nbsp;"
+                            >
+                                Skateboarding is not a hobby. And it's not a sport. Skateboarding is way of learning how
+                                to redefine the world around you. Ian Mackaye&nbsp;-&nbsp;
+                            </p>
+                        </div>
+                    </React.Fragment>
+                </Layout>
+            </Page>
+        );
+    }
+
+    private emailOnChange = (email) => {
+        this.setState({ email });
+    };
+
+    private handleSubmit = (event) => {
+        event.preventDefault();
+        const { email } = this.state;
+        if (!validator.validate(email)) {
+            this.setState({ subscribeError: 'This email is not valid' });
+        } else {
+            axios
+                .post(`https://barde.skatekrak.com/emails`, { email })
+                .then(() => this.setState({ subscribed: true, email: '' }))
+                .catch((err: AxiosError) => this.setState({ subscribeError: err.response.data.message }));
+        }
+    };
+}
 
 export default Index;
