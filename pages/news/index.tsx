65fbe00100441d1b02aa715f3f36432a1d0b8ef1
Arthur <a@skatekrak.com>
2018-12-07 15:48:54 +0000

    :sparkles: Add Articles and Menu to News

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 import Layout from 'components/Layout/Layout';
 import Page from 'components/pages/Page';
 
+import Articles from 'components/pages/news/Articles';
+import Menu from 'components/pages/news/Menu';
+
 type State = {};
 
 class News extends React.PureComponent<{}, State> {
@@ -14,7 +17,8 @@ class News extends React.PureComponent<{}, State> {
                 <Layout>
                     <React.Fragment>
                         <div id="news-container" className="container-fluid">
-                            HELLO WORLD, it's a fucking gold mine over there !
+                            <Menu />
+                            <Articles />
                         </div>
                     </React.Fragment>
                 </Layout>
