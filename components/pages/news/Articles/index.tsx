8717bb29619d073aa300b13547bdb2f55e0cf191
Arthur <a@skatekrak.com>
2018-12-07 15:48:07 +0000

    :sparkles: Create basic Articles and Menu component for News

---
+++ components/pages/news/Articles/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Articles: React.SFC = () => <div id="news-articles-container">ARTICLES</div>;
+
+export default Articles;
---
+++ components/pages/news/Menu/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Menu: React.SFC = () => (
+    <div id="news-menu-container">
+        <div id="news-menu-sources">
+            <div id="news-menu-sources-nav">
+                <span id="news-menu-sources-nav-title">Sources(38)</span>
+                <span id="news-menu-sources-nav-toggle-button">View all</span>
+            </div>
+        </div>
+    </div>
+);
+
+export default Menu;
