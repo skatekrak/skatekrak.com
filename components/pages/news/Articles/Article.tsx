2b08615f5c83a079e352bad4da2e42e03c582101
Arthur <a@skatekrak.com>
2018-12-11 16:40:40 +0000

    :sparkles: Add link to Article card

--- components/pages/news/Articles/Article.tsx
+++ components/pages/news/Articles/Article.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 
 const Article: React.SFC = () => (
     <div className="news-article col-xs-12 col-sm-6 col-lg-3">
-        <div className="news-article-cover-img-container">
-            <div className="news-article-cover-img" />
-        </div>
-        <h2 className="news-article-title">Who Should be the 2018 Skater of the Year?</h2>
+        <a href="" className="news-article-link">
+            <div className="news-article-cover-img-container">
+                <div className="news-article-cover-img" />
+            </div>
+            <h2 className="news-article-title">Who Should be the 2018 Skater of the Year?</h2>
+        </a>
         <div className="news-article-details">
             <div className="news-article-details-source">
                 by
-                <img src="" alt="" className="news-article-details-source-logo" />
-                <span className="news-article-details-source-name">Trasher Magazine</span>
+                <a href="" className="news-article-details-source-link">
+                    <img src="" alt="" className="news-article-details-source-logo" />
+                    <span className="news-article-details-source-name">Trasher Magazine</span>
+                </a>
             </div>
             <span className="news-article-details-date">&nbsp;- 9h ago</span>
         </div>
