6899057fac76884f692a92740f7d0438f32e690e
Arthur <a@skatekrak.com>
2018-12-13 12:26:52 +0000

    :sparkles: Create SpinnerCircle icon

---
+++ components/Ui/Icons/Spinners.tsx
@@ -0,0 +1,9 @@
+/* Author
+https://codepen.io/supah/pen/BjYLdW
+*/
+
+export const SpinnerCircle = () => (
+    <svg className="spinner-circle" viewBox="0 0 50 50" data-author="supahfunk">
+        <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
+    </svg>
+);
