4859c083e716d4b4034a1addfdb22f75226f38e9
Arthur <a@skatekrak.com>
2018-12-13 23:23:29 +0000

    :sparkles: Add ScrollTop to News

--- components/pages/news/Menu/index.tsx
+++ components/pages/news/Menu/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import Sources from 'components/pages/news/Menu/Sources';
+import ScrollTop from 'components/Ui/Utils/ScrollTop';
 
 type Props = {
     sourcesMenuIsOpen: boolean;
@@ -10,6 +11,7 @@ type Props = {
 const Menu: React.SFC<Props> = ({ sourcesMenuIsOpen, handleOpenSourcesMenu }) => (
     <div id="news-menu-container" className="col-xs-12 last-md col-md-4 col-lg-3">
         <Sources sourcesMenuIsOpen={sourcesMenuIsOpen} handleOpenSourcesMenu={handleOpenSourcesMenu} />
+        <ScrollTop elementId="news-menu-sources" />
     </div>
 );
 
