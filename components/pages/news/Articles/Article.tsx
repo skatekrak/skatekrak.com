f39f90c291b841f69505780aa32c79ea80e805af
Seiji Fouquet <sf@thepunkclub.com>
2018-12-17 23:41:12 +0000

    :lipstick: Add date ago formatting

--- components/pages/news/Articles/Article.tsx
+++ components/pages/news/Articles/Article.tsx
@@ -1,3 +1,4 @@
+import { distanceInWordsToNow } from 'date-fns';
 import * as React from 'react';
 import Truncate from 'react-truncate';
 import { Content } from 'types/Content';
@@ -14,7 +15,7 @@ class Article extends React.PureComponent<Props, State> {
 
         return (
             <div className="news-article col-xs-12 col-sm-6 col-lg-3">
-                <a href={content.webUrl} className="news-article-link">
+                <a href={content.webUrl} className="news-article-link" target="_blank" rel="noreferrer">
                     <div className="news-article-cover-img-container">
                         <div
                             style={{ backgroundImage: `url(${content.imageUrl})` }}
@@ -26,12 +27,17 @@ class Article extends React.PureComponent<Props, State> {
                 <div className="news-article-details">
                     <div className="news-article-details-source">
                         by
-                        <a href={content.source.webUrl} className="news-article-details-source-link">
+                        <a
+                            href={content.source.webUrl}
+                            className="news-article-details-source-link"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
                             <img src="" alt="" className="news-article-details-source-logo" />
                             <span className="news-article-details-source-name">{content.source.label}</span>
                         </a>
                     </div>
-                    <span className="news-article-details-date">&nbsp;- 9h ago</span>
+                    <span className="news-article-details-date">&nbsp;- {distanceInWordsToNow(content.createdAt)}</span>
                 </div>
                 <p className="news-article-desc">
                     <Truncate lines={4} ellipsis="...">
