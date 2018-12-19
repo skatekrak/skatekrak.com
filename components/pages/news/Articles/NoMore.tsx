ef7aaf7b97cb80dfea6305ee8b661e503613b889
Arthur <a@skatekrak.com>
2018-12-19 00:33:25 +0000

    :sparkles: Create NoMore in Article

---
+++ components/pages/news/Articles/NoMore.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoMore: React.SFC = () => (
+    <div key="no-more" id="news-articles-no-more">
+        No more content
+    </div>
+);
+
+export default NoMore;
