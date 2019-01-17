650bfded9bf253bb879c32107ab5fdd680950de4
Seiji Fouquet <sf@thepunkclub.com>
2019-01-17 16:25:45 +0000

    :sparkles: Add FeedLayout in setting reducer

--- components/pages/news/Articles/Article/index.tsx
+++ components/pages/news/Articles/Article/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Content } from 'rss-feed';
 
 import Ad from 'components/pages/news/Articles/Article/Ad';
 import Card from 'components/pages/news/Articles/Article/Card';
+import { FeedLayout, State as SettingState } from 'store/reducers/setting';
 
 type Props = {
     content: Content;
-    isAnAd: boolean;
+    index: number;
+    feedLayout: FeedLayout;
 };
 
-const Article: React.SFC<Props> = ({ content, isAnAd }: Props) => (
-    <div className="news-article col-xs-12 col-sm-6 col-lg-3">{!isAnAd ? <Card content={content} /> : <Ad />}</div>
-);
+type State = {
+    isClubPromotion: boolean;
+};
 
-export default Article;
+class Article extends React.PureComponent<Props, State> {
+    public state: State = {
+        isClubPromotion: false,
+    };
+
+    public componentDidMount() {
+        const { index, feedLayout } = this.props;
+        switch (feedLayout) {
+            case FeedLayout.OneColumn:
+                break;
+            case FeedLayout.TwoColumns:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public render() {
+        const { content } = this.props;
+        const { isClubPromotion } = this.state;
+
+        return (
+            <div className="news-article col-xs-12 col-sm-6 col-lg-3">
+                {!isClubPromotion ? <Card content={content} /> : <Ad />}
+            </div>
+        );
+    }
+}
+
+export default connect((state: { setting: SettingState }) => ({ feedLayout: state.setting.feedLayout }))(Article);
