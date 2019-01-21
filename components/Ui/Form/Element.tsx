aefad356bf78598883456877dde312adfdd20356
Arthur <a@skatekrak.com>
2019-01-21 15:00:20 +0000

    :art: Add withoutLabel prop on Form/Element

--- components/Ui/Form/Element.tsx
+++ components/Ui/Form/Element.tsx
@@ -4,22 +4,24 @@ import React from 'react';
 import 'static/styles/form.styl';
 
 type Props = {
-    label: string;
+    label?: string;
     children: React.ReactElement<React.ReactChildren>;
     invalid: boolean;
     valid?: boolean;
+    withoutLabel?: boolean;
 };
 
 export default class Element extends React.Component<Props> {
+    public static defaultProps = {
+        withoutLabel: false,
+    };
     public render() {
-        const { label, children, invalid, valid } = this.props;
+        const { label, children, invalid, valid, withoutLabel } = this.props;
         const elementId = `id-${children.props.name}`;
 
         return (
             <React.Fragment>
-                <div className="form-element-label">
-                    <label htmlFor={elementId}>{label}</label>
-                </div>
+                <div className="form-element-label">{!withoutLabel && <label htmlFor={elementId}>{label}</label>}</div>
                 <div
                     className={classNames('form-element-field', {
                         'form-element-field--error': invalid,
--- components/Ui/Form/render.tsx
+++ components/Ui/Form/render.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import FormElement from 'components/Ui/Form/Element';
 import ErrorMessage from 'components/Ui/Form/ErrorMessage';
 
-export const createRenderer = (render) => ({ input, meta, label, showValid, ...rest }) => (
+export const createRenderer = (render) => ({ input, meta, label, showValid, withoutLabel, ...rest }) => (
     <div className="form-element">
         <FormElement
             invalid={meta.error && meta.touched && !meta.active}
             valid={showValid && (!meta.pristine && !meta.error && !meta.active)}
             label={label}
+            withoutLabel={withoutLabel}
         >
             {render(input, rest)}
         </FormElement>
