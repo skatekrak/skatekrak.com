5671d61f1bfd82fb94b12cf92c7400a7d7e3b7cf
Arthur <a@skatekrak.com>
2019-01-21 19:35:45 +0000

    :lipstick: Add styles to Login

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -14,6 +14,7 @@ import 'static/styles/helpers.styl';
 import 'static/styles/main.styl';
 import 'static/styles/styleguide.styl';
 import 'static/styles/stylus-mq.styl';
+import 'static/styles/auth.styl';
 
 type Props = {
     head?: React.ReactNode;
