161ccdc096e822bf08c9b06ff1fade26723e402e
Arthur <a@skatekrak.com>
2019-01-22 18:11:59 +0000

    :sparkles: Create Authentication component

---
+++ components/pages/club/authentication/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { State as SettingState } from 'store/reducers/setting';
+
+import ForgotPassword from 'components/pages/club/authentication/forgotPassword';
+import Login from 'components/pages/club/authentication/login';
+
+import IconArrow from 'components/Ui/Icons/Arrow';
+import IconCross from 'components/Ui/Icons/Cross';
+
+interface Props {
+    setting: SettingState;
+    view: string;
+}
+
+type State = {};
+
+class ClubAuth extends React.PureComponent<Props, State> {
+    public render() {
+        return (
+            <>
+                <div className="auth-container container-fluid">
+                    {this.props.setting.isMobile ? (
+                        <span className="auth-back">
+                            <IconArrow />
+                            Back
+                        </span>
+                    ) : (
+                        <span className="auth-close">
+                            <IconCross />
+                        </span>
+                    )}
+                    {this.props.view === 'login' && <Login />}
+                    {this.props.view === 'forgot' && <ForgotPassword />}
+                </div>
+            </>
+        );
+    }
+}
+
+export default connect((state: any) => ({ setting: state.setting }))(ClubAuth);
