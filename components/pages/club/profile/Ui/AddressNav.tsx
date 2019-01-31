29b6d5fe3b683990cc4bf621e6da436ff1f110d9
Arthur <a@skatekrak.com>
2019-01-31 12:28:00 +0000

    :sparkles: Create Profile/Ui/AddressNav component

---
+++ components/pages/club/profile/Ui/AddressNav.tsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames';
+import React from 'react';
+
+import ButtonWithLabel from 'components/Ui/Button/buttonWithLabel';
+import IconStarFull from 'components/Ui/Icons/StarFull';
+import IconTrash from 'components/Ui/Icons/Trash';
+
+type Props = {
+    address: object;
+    onDeleteClick: (fct: any) => void;
+};
+
+const AddressNav: React.SFC<Props> = ({ address, onDeleteClick }) => (
+    <div className="profile-address-nav">
+        <span className="profile-address-nav-item">
+            {address.actual ? (
+                <span
+                    className={classNames('profile-address-nav-item-status', {
+                        'profile-address-nav-item-status--active': address.actual,
+                    })}
+                >
+                    <IconStarFull />
+                    Actual shipping address
+                </span>
+            ) : (
+                <button
+                    className={classNames('profile-address-nav-item-status', {
+                        'profile-address-nav-item-status--active': address.actual,
+                    })}
+                >
+                    <IconStarFull />
+                    Set as shipping address
+                </button>
+            )}
+        </span>
+        <ButtonWithLabel actionLabel="delete" content="address 1" icon={<IconTrash />} onClick={onDeleteClick} />
+    </div>
+);
+
+export default AddressNav;
