f391db80e13a2ce5befa9449a60b4347aefa7de0
Arthur <a@skatekrak.com>
2019-01-31 16:18:22 +0000

    :sparkles: Create Download icon component

---
+++ components/Ui/Icons/Download.tsx
@@ -0,0 +1,7 @@
+/* tslint:disable:max-line-length */
+export default () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+        <rect x="43.5" y="49.5" width="9" height="68" rx="2" ry="2" transform="translate(131.5 35.5) rotate(90)" />
+        <path d="M49.54,67.77,70.71,46.6a2,2,0,0,0,0-2.83l-3.54-3.54a2,2,0,0,0-2.83,0L52,52.58V10a2,2,0,0,0-2-2H45a2,2,0,0,0-2,2V51.34L31.9,40.23a2,2,0,0,0-2.83,0l-3.54,3.54a2,2,0,0,0,0,2.83L46.7,67.77a2,2,0,0,0,2.84,0Z" />
+    </svg>
+);
