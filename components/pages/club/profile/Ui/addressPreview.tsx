3efa8dc40b3a01caefc40e7f5c2794257819fb71
Arthur <a@skatekrak.com>
2019-01-31 12:27:38 +0000

    :sparkles: Create Profile/Ui/AddressPreview component

---
+++ components/pages/club/profile/Ui/addressPreview.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type Props = {
+    address: object;
+};
+
+const AddressPreview: React.SFC<Props> = ({ address }) => (
+    <div className="profile-address-preview">
+        <p className="profile-address-preview-line">
+            {address.firstName} {address.lastName}
+        </p>
+        <p className="profile-address-preview-line">
+            {address.street}, {address.apt}
+        </p>
+        <p className="profile-address-preview-line">
+            {address.cityCode} {address.city}
+        </p>
+        <p className="profile-address-preview-line">
+            {address.state}, {address.country}
+        </p>
+    </div>
+);
+
+export default AddressPreview;
