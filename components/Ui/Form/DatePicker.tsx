ac809728a8a11df42b0d3a4ddc56e5df6d1c6025
Maxime Cattet <m@skatekrak.com>
2019-02-07 18:53:27 +0000

    :sparkles: Add errors display on DatePicker

--- components/Ui/Form/DatePicker.tsx
+++ components/Ui/Form/DatePicker.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import DatePicker from 'react-datepicker';
 import { Field } from 'react-final-form';
 
+import ErrorMessage from 'components/Ui/Form/ErrorMessage';
+
 const DatePickerForm = (props: any) => (
     <Field name={props.name}>
         {({ input, meta }) => {
@@ -9,6 +11,12 @@ const DatePickerForm = (props: any) => (
             if (typeof input.value === 'string' && input.value !== '') {
                 date = new Date(input.value);
             }
+
+            let showError = (meta.error || meta.submitError) && meta.touched;
+            if (meta.dirtySinceLastSubmit) {
+                showError = false;
+            }
+
             return (
                 <div className="form-element">
                     {props.label && (
@@ -26,6 +34,7 @@ const DatePickerForm = (props: any) => (
                             dropdownMode="select"
                             className="form-control"
                         />
+                        {showError && <ErrorMessage message={meta.error || meta.submitError} />}
                     </div>
                 </div>
             );
