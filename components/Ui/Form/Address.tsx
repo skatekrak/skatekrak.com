27a5583a6ebf3c9ad4312a993525c13fbe00ea47
Maxime Cattet <m@skatekrak.com>
2019-02-14 18:08:24 +0000

    :sparkles: Update Address component
    - Can get countries from list
    - Can get countries from Sesterces for shipping

--- components/Ui/Form/Address.tsx
+++ components/Ui/Form/Address.tsx
@@ -1,22 +1,103 @@
+import gql from 'graphql-tag';
 import React from 'react';
 
+import withApollo, { WithApolloProps } from 'hocs/withApollo';
+
 import Field from 'components/Ui/Form/Field';
+import Select from 'components/Ui/Form/Select';
 
-const Address: React.SFC = () => (
-    <React.Fragment>
-        <div className="form-double-field-line">
-            <Field name="First name" placeholder="First name" />
-            <Field name="Last name" placeholder="Last name" />
-        </div>
-        <Field name="Address" placeholder="Address" />
-        <Field name="Apt/unit etc (optional)" placeholder="Apt/unit etc (optional)" />
-        <div className="form-double-field-line">
-            <Field name="City" placeholder="City" />
-            <Field name="Postal code" placeholder="Postal code" />
-        </div>
-        <Field name="State" placeholder="State" />
-        <Field name="Country" placeholder="Country" />
-    </React.Fragment>
-);
+import countries from 'lib/countries';
+import createPropsGetter from 'lib/getProps';
 
-export default Address;
+const GET_COUNTRIES = gql`
+    query {
+        countries {
+            isoCode
+            name
+        }
+    }
+`;
+
+type Props = {
+    namePrefix?: string;
+} & Partial<DefaultProps>;
+
+type DefaultProps = Readonly<typeof defaultProps>;
+
+const defaultProps = {
+    allCountries: true,
+};
+
+const getProps = createPropsGetter(defaultProps);
+
+class Address extends React.PureComponent<Props & WithApolloProps> {
+    public render() {
+        const { namePrefix, allCountries } = getProps(this.props);
+        return (
+            <>
+                <div className="form-double-field-line">
+                    <Field name={getName('firstName', namePrefix)} placeholder="First name" />
+                    <Field name={getName('lastName', namePrefix)} placeholder="Last name" />
+                </div>
+                <Field name={getName('line1', namePrefix)} placeholder="Street" />
+                <Field name={getName('line2', namePrefix)} placeholder="Apt/unit etc (optional)" />
+                <div className="form-double-field-line">
+                    <Field name={getName('city', namePrefix)} placeholder="City" />
+                    <Field name={getName('postalCode', namePrefix)} placeholder="Postal code" />
+                </div>
+                <Field name={getName('state', namePrefix)} placeholder="State" />
+                {allCountries ? (
+                    <Select
+                        name={getName('country', namePrefix)}
+                        placeholder="Country"
+                        options={formatCountries(countries)}
+                        menuMaxHeight={200}
+                        isSearchable
+                    />
+                ) : (
+                    <Select
+                        name={getName('country', namePrefix)}
+                        placeholder="Country"
+                        loadOptions={this.queryCountries}
+                        menuMaxHeight={200}
+                    />
+                )}
+            </>
+        );
+    }
+
+    private queryCountries = (_inputValue: string, callback: (options: {}[]) => void) => {
+        this.props.apolloClient
+            .query<any>({
+                query: GET_COUNTRIES,
+            })
+            .then((result) => {
+                const options = result.data.countries.map((country) => ({
+                    label: country.name,
+                    value: country.isoCode,
+                }));
+
+                callback(options);
+            });
+    };
+}
+
+const getName = (name: string, prefix?: string): string => {
+    if (prefix) {
+        return `${prefix}.${name}`;
+    }
+    return name;
+};
+
+const formatCountries = (cntrs: typeof countries): { label: string; value: string }[] => {
+    const options: { label: string; value: string }[] = [];
+    for (const key of Object.keys(cntrs)) {
+        options.push({
+            label: cntrs[key],
+            value: key.toLowerCase(),
+        });
+    }
+    return options;
+};
+
+export default withApollo(Address);
