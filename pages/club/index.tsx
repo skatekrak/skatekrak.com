9e966b29a565b2d6b23c0127c1b623762d0c2117
Arthur <a@skatekrak.com>
2019-02-16 12:29:47 +0000

    :sparkles: Add youtube player to club presentation

--- pages/club/index.tsx
+++ pages/club/index.tsx
@@ -1,6 +1,7 @@
 import Analytics, { ABTest, Variation } from '@thepunkclub/analytics';
 import Head from 'next/head';
 import React from 'react';
+import ReactPlayer from 'react-player';
 import { connect } from 'react-redux';
 
 import Layout from 'components/Layout/Layout';
@@ -171,19 +172,43 @@ class Club extends React.Component<Props, State> {
                                 <ul id="club-main-youtube-videos" className="row">
                                     <li className="club-main-youtube-item col-xs-12 col-sm-6 col-md-4">
                                         <div className="club-main-youtube-item-player-container">
-                                            <div className="club-main-youtube-item-player" />
+                                            <div className="club-main-youtube-item-player">
+                                                <ReactPlayer
+                                                    url="https://www.youtube.com/watch?v=cdghhu3Yj8A"
+                                                    controls
+                                                    height="100%"
+                                                    width="100%"
+                                                    light
+                                                />
+                                            </div>
                                         </div>
                                         <h3 className="club-main-youtube-item-title">Best of #4</h3>
                                     </li>
                                     <li className="club-main-youtube-item col-xs-12 col-sm-6 col-md-4">
                                         <div className="club-main-youtube-item-player-container">
-                                            <div className="club-main-youtube-item-player" />
+                                            <div className="club-main-youtube-item-player">
+                                                <ReactPlayer
+                                                    url="https://www.youtube.com/watch?v=BvdDTThUzME"
+                                                    controls
+                                                    height="100%"
+                                                    width="100%"
+                                                    light
+                                                />
+                                            </div>
                                         </div>
                                         <h3 className="club-main-youtube-item-title">Best of #3</h3>
                                     </li>
                                     <li className="club-main-youtube-item col-xs-12 col-sm-6 col-md-4">
                                         <div className="club-main-youtube-item-player-container">
-                                            <div className="club-main-youtube-item-player" />
+                                            <div className="club-main-youtube-item-player">
+                                                <ReactPlayer
+                                                    url="https://www.youtube.com/watch?v=D2yrZ-i-4WU"
+                                                    controls
+                                                    height="100%"
+                                                    width="100%"
+                                                    light
+                                                />
+                                            </div>
                                         </div>
                                         <h3 className="club-main-youtube-item-title">Best of #2</h3>
                                     </li>
