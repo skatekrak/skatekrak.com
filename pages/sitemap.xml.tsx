903e302bb5ecab114214abc1d8332c9a358cc539
Seiji Fouquet <sf@thepunkclub.com>
2019-02-28 23:17:16 +0000

    :sparkles: Add robots.txt and sitemap.xml pages

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,40 @@
+import format from 'date-fns/format';
+import React from 'react';
+
+const lastMod = format(new Date(), 'YYYY-MM-DD');
+
+const sitemapXml = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://skatekrak.com</loc>
+        <lastmod>${lastMod}</lastmod>
+        <changefreq>daily</changefreq>
+    </url>
+    <url>
+        <loc>https://skatekrak.com/club</loc>
+        <lastmod>${lastMod}</lastmod>
+        <changefreq>daily</changefreq>
+    </url>
+    <url>
+        <loc>https://skatekrak.com/news</loc>
+        <lastmod>${lastMod}</lastmod>
+        <changefreq>hourly</changefreq>
+    </url>
+    <url>
+        <loc>https://skatekrak.com/auth/login</loc>
+        <lastmod>${lastMod}</lastmod>
+        <changefreq>daily</changefreq>
+    </url>
+    <url>
+        <loc>https://mag.skatekrak.com</loc>
+        <lastmod>${lastMod}</lastmod>
+        <changefreq>hourly</changefreq>
+    </url>
+</urlset>`;
+
+export default class Sitemap extends React.Component {
+    public static getInitialProps({ res }) {
+        res.setHeader('Content-Type', 'text/xml');
+        res.write(sitemapXml);
+        res.end();
+    }
+}
