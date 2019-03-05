ca500b0e5e865ff5c07ec9e3b823eeb6d262ada0
Arthur <a@skatekrak.com>
2019-03-05 17:53:38 +0000

    :sparkles: Create LayoutFeed component

---
+++ components/Ui/Layout/LayoutFeed.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {
+    mainView: React.ReactNode;
+    sideBar: React.ReactNode;
+};
+
+const LayoutFeed: React.SFC<Props> = ({ mainView, sideBar }) => (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-xs-12 last-md col-md-4 col-lg-3">{sideBar}</div>
+            <div className="col-xs-12 col-md-8 col-lg-9">{mainView}</div>
+        </div>
+    </div>
+);
+
+export default LayoutFeed;
