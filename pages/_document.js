3215ea5fa98b3fbeba12d75a48d63f9808461edd
Maxime Cattet <m@skatekrak.com>
2019-03-07 10:41:42 +0000

    :chart_with_upwards_trend: Add google adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,13 @@ export default class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon_16.png" />
                     <script src="https://js.stripe.com/v3" />
                     <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" />
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({
+                        google_ad_client: "ca-pub-8520381468370007",
+                        enable_page_level_ads: true
+                    });
+                    </script>
                 </Head>
                 <body>
                     <Main />
