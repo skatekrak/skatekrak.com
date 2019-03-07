012e96efd325b5b5844d648460fa014ddb937d00
Arthur <a@skatekrak.com>
2019-03-07 14:01:27 +0000

    :lipstick: Put real wording and improve style on Video Menu

--- components/pages/videos/Menu/index.tsx
+++ components/pages/videos/Menu/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
 import ScrollTop from 'components/Ui/Utils/ScrollTop';
+import Emoji from 'components/Ui/Icons/Emoji';
 
 const Menu: React.SFC = () => (
     <div id="videos-menu-container">
         <div id="videos-menu-desc">
             <h1 id="videos-menu-desc-title">
-                Your daily dose
-                <br />
-                of Krak vidz
+                We all need our weekly dose{' '}
+                <span id="videos-menu-desc-subtitle">
+                    [at least] <Emoji symbol="🐙" label="Kraken" /> <Emoji symbol="📺" label="television" />
+                </span>
             </h1>
             <p id="videos-menu-desc-text">
-                Krak brings you all the daily videos you need to see. The best of skateboarding vidz at the same place.
-                On peut mettre ce qu’on veut ici pas forcement du texte, on rien du tout. Enjoy the show 🤘
+                We love filming skateboarding. Period. We make things for skateboarders and with these products, our
+                community of riders is exploring the world out there.
             </p>
         </div>
         <ScrollTop elementId="videos-menu-desc" />
