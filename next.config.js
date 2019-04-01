6a2d1e0aae58be45d61a35ba8d27d507d0afdb08
Seiji Fouquet <sf@thepunkclub.com>
2019-04-01 22:53:22 +0100

    :wrench: Use .env file in CI docker-compose

--- next.config.js
+++ next.config.js
@@ -3,11 +3,22 @@ const withStylus = require('@zeit/next-stylus');
 const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 const webpack = require('webpack');
-const { parsed: localEnv } = require('dotenv').config();
 
 module.exports = withPlugins([withTypescript, withCSS, withStylus], {
     webpack: (config, options) => {
-        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+        config.plugins.push(
+            new webpack.EnvironmentPlugin([
+                'STRIPE_KEY',
+                'CAIROTE_URL',
+                'SESTERCES_URL',
+                'RSS_BACKEND_URL',
+                'INTERCOM_ID',
+                'CACHING_URL',
+                'REDIRECT_URL',
+                'RENEW_DATE',
+                'RENEW_DATE_QUARTERFULL',
+            ]),
+        );
         return config;
     },
 });
