14baca1102c2a175489157f0e1743ecf891210a7
Arthur <a@skatekrak.com>
2019-05-23 11:16:00 +0100

    :sparkles: Add details to VideoCards and featured

--- components/pages/videos/VideoFeed/Video/VideoCard.tsx
+++ components/pages/videos/VideoFeed/Video/VideoCard.tsx
@@ -1,3 +1,4 @@
+import { distanceInWordsToNow } from 'date-fns';
 import React from 'react';
 
 import VideoPlayer from 'components/Ui/Player/VideoPlayer';
@@ -16,6 +17,12 @@ class VideoCard extends React.PureComponent<Props, State> {
         return (
             <div className="video-card">
                 <VideoPlayer url={`https://www.youtube.com/watch?v=${video.videoId}`} controls light playing />
+                <div className="video-card-details">
+                    <p className="video-card-details-source">
+                        by {video.source.title}
+                        <span className="video-card-details-date">&nbsp;- {distanceInWordsToNow(video.createdAt)}</span>
+                    </p>
+                </div>
                 <h2 className="video-card-title">{video.title}</h2>
             </div>
         );
