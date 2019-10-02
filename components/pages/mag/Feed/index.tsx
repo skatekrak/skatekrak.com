3694d50d884ea0cca0632a128a1b6f4124371a78
Arthur <a@skatekrak.com>
2019-10-02 18:02:18 +0100

    :recycle: Refactor how to get Posts from wp api
    update/delete some api call

--- components/pages/mag/Feed/index.tsx
+++ components/pages/mag/Feed/index.tsx
@@ -1,25 +1,30 @@
 import axios from 'axios';
 import classNames from 'classnames';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import TrackedPage from 'components/pages/TrackedPage';
 
 import Card from 'components/pages/mag/Feed/Card';
 import { KrakLoading } from 'components/Ui/Icons/Spinners';
-// import ScrollHelper from 'lib/ScrollHelper';
+import ScrollHelper from 'lib/ScrollHelper';
 
 export interface Post {
-    id: number;
-    title: {
-        rendered: string;
+    id?: number;
+    title?: {
+        rendered?: string;
     };
-    slug: string;
-    link: string;
-    date: string;
-    featured_media: number;
-    thumbnailImage: string;
-    categories: any[];
-    categoriesString: string;
+    slug?: string;
+    link?: string;
+    date?: string;
+    content?: {
+        rendered?: string;
+    };
+    featured_media?: number;
+    thumbnailImage?: string;
+    categories?: any[];
+    categoriesString?: string;
+    _embedded?: object;
 }
 
 type Props = {
@@ -28,13 +33,17 @@ type Props = {
 
 type State = {
     posts?: Post[];
+    page: number;
     isLoading: boolean;
+    hasMore: boolean;
 };
 
 class Feed extends React.Component<Props, State> {
     public state: State = {
         posts: [],
+        page: 1,
         isLoading: false,
+        hasMore: true,
     };
 
     public async componentDidMount() {
@@ -42,20 +51,29 @@ class Feed extends React.Component<Props, State> {
     }
 
     public render() {
-        const { posts, isLoading } = this.state;
+        const { posts, isLoading, hasMore } = this.state;
 
         return (
             <div id="mag-feed">
                 <TrackedPage name={`Mag/${Math.ceil(posts.length / 20)}`} initial={false} />
-                <div className={classNames('row', { hide: this.props.sidebarNavIsOpen })}>
-                    {posts.length !== 0 &&
-                        posts.map((post) => (
-                            <div key={post.id} className="mag-card-container col-xs-12 col-sm-6 col-lg-4">
-                                <Card post={post} />
-                            </div>
-                        ))}
-                    {isLoading && <KrakLoading />}
-                </div>
+                <InfiniteScroll
+                    pageStart={1}
+                    initialLoad={false}
+                    loadMore={this.loadMore}
+                    hasMore={!isLoading && hasMore}
+                    getScrollParent={this.getScrollContainer}
+                    useWindow={false}
+                >
+                    <div className={classNames('row', { hide: this.props.sidebarNavIsOpen })}>
+                        {posts.length !== 0 &&
+                            posts.map((post) => (
+                                <div key={post.id} className="mag-card-container col-xs-12 col-sm-6 col-lg-4">
+                                    <Card post={post} />
+                                </div>
+                            ))}
+                        {isLoading && <KrakLoading />}
+                    </div>
+                </InfiniteScroll>
             </div>
         );
     }
@@ -65,7 +83,13 @@ class Feed extends React.Component<Props, State> {
 
         try {
             let req: Promise<any>;
-            req = axios.get('https://mag.skatekrak.com/wp-json/wp/v2/posts');
+            if (this.state.posts.length !== 0) {
+                req = axios.get(
+                    `https://mag.skatekrak.com/wp-json/wp/v2/posts?per_page=20&before=${this.state.posts[this.state.posts.length - 1].date}&_embed`,
+                );
+            } else {
+                req = axios.get(`https://mag.skatekrak.com/wp-json/wp/v2/posts?per_page=20&page=1&_embed`);
+            }
             const res = await req;
 
             if (res.data) {
@@ -79,7 +103,10 @@ class Feed extends React.Component<Props, State> {
         } catch (err) {
             // console.log(err);
         } finally {
-            this.setState({ isLoading: false });
+            this.setState({
+                isLoading: false,
+                page: this.state.page += 1,
+            });
         }
     };
 
@@ -89,13 +116,10 @@ class Feed extends React.Component<Props, State> {
         for (const post of posts) {
             // Get formated categories
             if (post.categories) {
-                const categories = post.categories;
+                const categories = post._embedded['wp:term'][0];
                 let formatedCategories = '';
                 for (let iCategory = 0; iCategory < categories.length; iCategory++) {
-                    const res = await axios.get(
-                        `https://mag.skatekrak.com/wp-json/wp/v2/categories/${categories[iCategory]}`,
-                    );
-                    const categoryName = res.data.name;
+                    const categoryName = categories[iCategory].name;
                     formatedCategories += categoryName;
                     if (iCategory !== categories.length - 1) {
                         formatedCategories += ', ';
@@ -106,22 +130,18 @@ class Feed extends React.Component<Props, State> {
 
             // Get image
             if (post.featured_media) {
-                const resMedia = await axios.get(
-                    `https://mag.skatekrak.com/wp-json/wp/v2/media/${post.featured_media}`,
-                );
-                if (resMedia.data.media_details.sizes.medium_large) {
-                    const thumbnailImage = resMedia.data.media_details.sizes.medium_large.source_url;
-                    post.thumbnailImage = thumbnailImage;
-                } else if (resMedia.data.media_details.sizes.medium) {
-                    const thumbnailImage = resMedia.data.media_details.sizes.medium.source_url;
-                    post.thumbnailImage = thumbnailImage;
-                }
+                const thumbnailImage = post._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url;
+                post.thumbnailImage = thumbnailImage;
             }
 
             formatedPosts.push(post);
         }
 
         return formatedPosts;
+    };
+
+    private getScrollContainer = () => {
+        return ScrollHelper.getScrollContainer();
     };
 }
 
