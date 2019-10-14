d8476ecc09e0072d85cd8597d742b06232bafce9
Maxime Cattet <m@skatekrak.com>
2019-10-14 18:26:54 +0200

    :bug: Fix apollo for countries query

--- components/Ui/Form/Address.tsx
+++ components/Ui/Form/Address.tsx
@@ -29,55 +29,53 @@ const defaultProps = {
 
 const getProps = createPropsGetter(defaultProps);
 
-class Address extends React.PureComponent<Props> {
-    public render() {
-        const { namePrefix, allCountries } = getProps(this.props);
-        return (
-            <>
-                <div className="form-double-field-line">
-                    <Field name={getName('firstName', namePrefix)} placeholder="First name" />
-                    <Field name={getName('lastName', namePrefix)} placeholder="Last name" />
-                </div>
-                <Field name={getName('line1', namePrefix)} placeholder="Street" />
-                <Field name={getName('line2', namePrefix)} placeholder="Apt/unit etc (optional)" />
-                <div className="form-double-field-line">
-                    <Field name={getName('city', namePrefix)} placeholder="City" />
-                    <Field name={getName('postalCode', namePrefix)} placeholder="Postal code" />
-                </div>
-                <Field name={getName('state', namePrefix)} placeholder="State" />
-                {allCountries ? (
-                    <Select
-                        name={getName('country', namePrefix)}
-                        placeholder="Country"
-                        options={formatCountries(countries)}
-                        menuMaxHeight={200}
-                        isSearchable
-                    />
-                ) : (
-                    <Select
-                        name={getName('country', namePrefix)}
-                        placeholder="Country"
-                        loadOptions={this.queryCountries}
-                        menuMaxHeight={200}
-                    />
-                )}
-            </>
-        );
+const Address = (props: Props) => {
+    const { namePrefix, allCountries } = getProps(props);
+
+    const { loading, data } = useQuery(GET_COUNTRIES);
+
+    let queriedCountries = [];
+
+    if (data) {
+        queriedCountries = data.countries.map(country => ({
+            label: country.name,
+            value: country.isoCode,
+        }));
     }
 
-    private queryCountries = (_inputValue: string, callback: (options: {}[]) => void) => {
-        useQuery(GET_COUNTRIES, {
-            onCompleted: result => {
-                const options = result.data.countries.map(country => ({
-                    label: country.name,
-                    value: country.isoCode,
-                }));
-
-                callback(options);
-            },
-        });
-    };
-}
+    return (
+        <>
+            <div className="form-double-field-line">
+                <Field name={getName('firstName', namePrefix)} placeholder="First name" />
+                <Field name={getName('lastName', namePrefix)} placeholder="Last name" />
+            </div>
+            <Field name={getName('line1', namePrefix)} placeholder="Street" />
+            <Field name={getName('line2', namePrefix)} placeholder="Apt/unit etc (optional)" />
+            <div className="form-double-field-line">
+                <Field name={getName('city', namePrefix)} placeholder="City" />
+                <Field name={getName('postalCode', namePrefix)} placeholder="Postal code" />
+            </div>
+            <Field name={getName('state', namePrefix)} placeholder="State" />
+            {allCountries ? (
+                <Select
+                    name={getName('country', namePrefix)}
+                    placeholder="Country"
+                    options={formatCountries(countries)}
+                    menuMaxHeight={200}
+                    isSearchable
+                />
+            ) : (
+                <Select
+                    name={getName('country', namePrefix)}
+                    placeholder="Country"
+                    options={queriedCountries}
+                    menuMaxHeight={200}
+                    isLoading={loading}
+                />
+            )}
+        </>
+    );
+};
 
 const getName = (name: string, prefix?: string): string => {
     if (prefix) {
--- components/pages/club/subscribe/steps/createAccount.tsx
+++ components/pages/club/subscribe/steps/createAccount.tsx
@@ -3,6 +3,7 @@ import { FORM_ERROR } from 'final-form';
 import gql from 'graphql-tag';
 import getConfig from 'next/config';
 import React from 'react';
+import { useLazyQuery } from 'react-apollo';
 import { Form, FormSpy } from 'react-final-form';
 import { connect } from 'react-redux';
 
@@ -14,7 +15,6 @@ import Field from 'components/Ui/Form/Field';
 import { updateFormState } from 'store/form/actions';
 
 import { getPricingText } from 'lib/moneyHelper';
-import { useQuery } from 'react-apollo';
 
 type Props = {
     onNextClick: () => void;
@@ -25,94 +25,11 @@ type Props = {
     };
 };
 
-class CreateAccount extends React.Component<Props> {
-    public render() {
-        const { payment } = this.props;
-        const quarterFull: boolean = getConfig().publicRuntimeConfig.IS_QUARTERFULL;
-        return (
-            <Form onSubmit={this.handleSubmit} validate={validateForm}>
-                {({ handleSubmit, submitting, submitError }) => (
-                    <form className="modal-two-col-container modal-two-col-form" onSubmit={handleSubmit}>
-                        <FormSpy onChange={this.onFormChange} />
-                        <div className="modal-two-col-first-container">
-                            <article id="subscribe-promote">
-                                <header id="subscribe-promote-header">
-                                    <img
-                                        src="/static/images/club/club-hero-logo.svg"
-                                        alt="Krak skate club"
-                                        id="subscribe-promote-header-logo"
-                                    />
-                                    <h3 id="subscribe-promote-header-subtitle">Quarterly membership</h3>
-                                </header>
-                                <main id="subscribe-promote-main">
-                                    <p id="subscribe-promote-main-price">
-                                        {getPricingText(String(payment.price / 100), payment.currency)} today
-                                    </p>
-                                    {!quarterFull ? (
-                                        <>
-                                            <p id="subscribe-promote-main-cover">
-                                                to be covered until {getConfig().publicRuntimeConfig.NEXT_QUARTER_START}
-                                            </p>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <p id="subscribe-promote-main-cover">
-                                                to guarantee your slot for the next batch
-                                            </p>
-                                        </>
-                                    )}
-                                </main>
-                                <footer id="subscribe-promote-footer">
-                                    <p id="subscribe-promote-footer-limited">
-                                        Limited quantities available.
-                                        <br />
-                                        First come first served.
-                                    </p>
-                                </footer>
-                            </article>
-                        </div>
-                        <div className="modal-two-col-second-container modal-two-col-item-container">
-                            <h1 className="modal-two-col-title">
-                                Create
-                                <br />
-                                your account
-                            </h1>
-                            <div className="modal-two-col-content">
-                                <p className="modal-two-col-content-description">
-                                    {!quarterFull
-                                        ? 'Become a Kraken.'
-                                        : `Be sure to become a Kraken on ${
-                                              getConfig().publicRuntimeConfig.NEXT_QUARTER_START
-                                          }.`}
-                                </p>
-                                <p className="modal-two-col-content-description-nb">
-                                    nb: we might send you few surprises before the official starting date - yep, that’s
-                                    what it means to be a kraken!
-                                </p>
-                                <div className="form-double-field-line">
-                                    <Field name="firstName" placeholder="First name" />
-                                    <Field name="lastName" placeholder="Last name" />
-                                </div>
-                                <Field name="email" placeholder="Email" type="email" />
-                                <Field name="password" placeholder="Password" type="password" />
-                            </div>
-                            <ErrorMessage message={submitError} />
-                            <button
-                                type="submit"
-                                className="button-primary modal-two-col-form-submit"
-                                disabled={submitting}
-                            >
-                                {!quarterFull ? 'Become a Kraken' : 'Pre-pay'}
-                            </button>
-                        </div>
-                    </form>
-                )}
-            </Form>
-        );
-    }
+const CreateAccount = (props: Props) => {
+    const [checkEmail, { loading, data, error }] = useLazyQuery(CHECK_EMAIL);
 
-    private handleSubmit = async (values: any) => {
-        const { data, loading, error } = useQuery(CHECK_EMAIL, { variables: { email: values.email } });
+    const submit = async (values: any) => {
+        checkEmail({ variables: { email: values.email } });
 
         if (loading) {
             return;
@@ -122,16 +39,99 @@ class CreateAccount extends React.Component<Props> {
             return { [FORM_ERROR]: 'Oops, something went wrong, try later or contact us' };
         }
 
-        if ((data as any).checkEmail) {
+        if (data && data.checkEmail) {
             return { email: 'This email is already used' };
         }
-        this.props.onNextClick();
+        props.onNextClick();
     };
 
-    private onFormChange = state => {
-        this.props.updateFormState('account', state.values);
+    const onFormChange = state => {
+        props.updateFormState('account', state.values);
     };
-}
+
+    const { payment } = props;
+    const quarterFull: boolean = getConfig().publicRuntimeConfig.IS_QUARTERFULL;
+    return (
+        <Form onSubmit={submit} validate={validateForm}>
+            {({ handleSubmit, submitting, submitError }) => (
+                <form className="modal-two-col-container modal-two-col-form" onSubmit={handleSubmit}>
+                    <FormSpy onChange={onFormChange} />
+                    <div className="modal-two-col-first-container">
+                        <article id="subscribe-promote">
+                            <header id="subscribe-promote-header">
+                                <img
+                                    src="/static/images/club/club-hero-logo.svg"
+                                    alt="Krak skate club"
+                                    id="subscribe-promote-header-logo"
+                                />
+                                <h3 id="subscribe-promote-header-subtitle">Quarterly membership</h3>
+                            </header>
+                            <main id="subscribe-promote-main">
+                                <p id="subscribe-promote-main-price">
+                                    {getPricingText(String(payment.price / 100), payment.currency)} today
+                                </p>
+                                {!quarterFull ? (
+                                    <>
+                                        <p id="subscribe-promote-main-cover">
+                                            to be covered until {getConfig().publicRuntimeConfig.NEXT_QUARTER_START}
+                                        </p>
+                                    </>
+                                ) : (
+                                    <>
+                                        <p id="subscribe-promote-main-cover">
+                                            to guarantee your slot for the next batch
+                                        </p>
+                                    </>
+                                )}
+                            </main>
+                            <footer id="subscribe-promote-footer">
+                                <p id="subscribe-promote-footer-limited">
+                                    Limited quantities available.
+                                    <br />
+                                    First come first served.
+                                </p>
+                            </footer>
+                        </article>
+                    </div>
+                    <div className="modal-two-col-second-container modal-two-col-item-container">
+                        <h1 className="modal-two-col-title">
+                            Create
+                            <br />
+                            your account
+                        </h1>
+                        <div className="modal-two-col-content">
+                            <p className="modal-two-col-content-description">
+                                {!quarterFull
+                                    ? 'Become a Kraken.'
+                                    : `Be sure to become a Kraken on ${
+                                          getConfig().publicRuntimeConfig.NEXT_QUARTER_START
+                                      }.`}
+                            </p>
+                            <p className="modal-two-col-content-description-nb">
+                                nb: we might send you few surprises before the official starting date - yep, that’s what
+                                it means to be a kraken!
+                            </p>
+                            <div className="form-double-field-line">
+                                <Field name="firstName" placeholder="First name" />
+                                <Field name="lastName" placeholder="Last name" />
+                            </div>
+                            <Field name="email" placeholder="Email" type="email" />
+                            <Field name="password" placeholder="Password" type="password" />
+                        </div>
+                        <ErrorMessage message={submitError} />
+                        <button
+                            type="submit"
+                            className="button-primary modal-two-col-form-submit"
+                            disabled={submitting}
+                        >
+                            {!quarterFull ? 'Become a Kraken' : 'Pre-pay'}
+                        </button>
+                    </div>
+                </form>
+            )}
+        </Form>
+    );
+};
 
 const validateForm = (values: any) => {
     const errors: any = {};
