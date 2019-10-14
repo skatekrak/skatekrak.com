1759ade8f608453abfc0bcff6eca86866341d129
Arthur <a@skatekrak.com>
2019-10-14 13:27:52 +0100

    :recycle: Change useless class into dumb components

--- components/pages/mag/Feed/Article/index.tsx
+++ components/pages/mag/Feed/Article/index.tsx
@@ -18,69 +18,57 @@ type Props = {
     sidebarNavIsOpen: boolean;
 };
 
-class Article extends React.PureComponent<Props> {
-    public render() {
-        const { post, isLoading, sidebarNavIsOpen } = this.props;
-
-        return (
-            <>
-                {isLoading && <KrakLoading />}
-                {post && (
-                    <article id="mag-article" className={classNames({ hide: sidebarNavIsOpen })}>
-                        <div id="mag-article-actions">
-                            <Link href="/mag">
-                                <a id="mag-article-back">Back to the mag</a>
-                            </Link>
-                            <div id="mag-article-share">
-                                <span id="mag-article-share-text">Share on:</span>
-                                <FacebookShareButton
-                                    url={`https://skatekrak.com/mag/${post.slug}`}
-                                    quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
-                                >
-                                    <FacebookIcon size={24} round />
-                                </FacebookShareButton>
-                                <TwitterShareButton
-                                    url={`https://skatekrak.com/mag/${post.slug}`}
-                                    title={decodeHTML(post.title.rendered)}
-                                    via="skatekrak"
-                                >
-                                    <TwitterIcon size={24} round />
-                                </TwitterShareButton>
-                                <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
-                            </div>
-                        </div>
-                        <header id="mag-article-header">
-                            <p id="mag-article-category">{post.categoriesString}</p>
-                            <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title.rendered)} />
-                            <p id="mag-article-date">
-                                {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
-                            </p>
-                        </header>
-                        <main id="mag-article-main">
-                            {post._format_video_embed ? (
-                                <div
-                                    id="mag-article-video"
-                                    dangerouslySetInnerHTML={createMarkup(post._format_video_embed)}
-                                />
-                            ) : (
-                                <div id="mag-article-cover-img-container">
-                                    <div
-                                        id="mag-article-cover-img"
-                                        style={{ backgroundImage: `url("${post.featuredImageFull}")` }}
-                                    />
-                                </div>
-                            )}
+const Article: React.SFC<Props> = ({ post, isLoading, sidebarNavIsOpen }: Props) => (
+    <>
+        {isLoading && <KrakLoading />}
+        {post && (
+            <article id="mag-article" className={classNames({ hide: sidebarNavIsOpen })}>
+                <div id="mag-article-actions">
+                    <Link href="/mag">
+                        <a id="mag-article-back">Back to the mag</a>
+                    </Link>
+                    <div id="mag-article-share">
+                        <span id="mag-article-share-text">Share on:</span>
+                        <FacebookShareButton
+                            url={`https://skatekrak.com/mag/${post.slug}`}
+                            quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
+                        >
+                            <FacebookIcon size={24} round />
+                        </FacebookShareButton>
+                        <TwitterShareButton
+                            url={`https://skatekrak.com/mag/${post.slug}`}
+                            title={decodeHTML(post.title.rendered)}
+                            via="skatekrak"
+                        >
+                            <TwitterIcon size={24} round />
+                        </TwitterShareButton>
+                        <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
+                    </div>
+                </div>
+                <header id="mag-article-header">
+                    <p id="mag-article-category">{post.categoriesString}</p>
+                    <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title.rendered)} />
+                    <p id="mag-article-date">
+                        {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
+                    </p>
+                </header>
+                <main id="mag-article-main">
+                    {post._format_video_embed ? (
+                        <div id="mag-article-video" dangerouslySetInnerHTML={createMarkup(post._format_video_embed)} />
+                    ) : (
+                        <div id="mag-article-cover-img-container">
                             <div
-                                id="mag-article-content"
-                                dangerouslySetInnerHTML={createMarkup(post.content.rendered)}
+                                id="mag-article-cover-img"
+                                style={{ backgroundImage: `url("${post.featuredImageFull}")` }}
                             />
-                            <Comment />
-                        </main>
-                    </article>
-                )}
-            </>
-        );
-    }
-}
+                        </div>
+                    )}
+                    <div id="mag-article-content" dangerouslySetInnerHTML={createMarkup(post.content.rendered)} />
+                    <Comment />
+                </main>
+            </article>
+        )}
+    </>
+);
 
 export default Article;
--- components/pages/mag/Feed/Card/index.tsx
+++ components/pages/mag/Feed/Card/index.tsx
@@ -13,56 +13,46 @@ type Props = {
     post: Post;
 };
 
-type State = {};
-
-class Card extends React.PureComponent<Props, State> {
-    public render() {
-        const { post } = this.props;
-        return (
-            <>
-                <Link href={`/mag/${post.slug}`}>
-                    <a className="mag-card-link">
-                        <div className="mag-card-cover-img-container">
-                            <div
-                                className="mag-card-cover-img"
-                                style={{ backgroundImage: `url("${post.thumbnailImage}")` }}
-                            />
-                        </div>
-                    </a>
-                </Link>
-                <div className="mag-card-share">
-                    <FacebookShareButton
-                        url={`https://skatekrak.com/mag/${post.slug}`}
-                        quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
-                    >
-                        <FacebookIcon size={24} round />
-                    </FacebookShareButton>
-                    <TwitterShareButton
-                        url={`https://skatekrak.com/mag/${post.slug}`}
-                        title={decodeHTML(post.title.rendered)}
-                        via="skatekrak"
-                    >
-                        <TwitterIcon size={24} round />
-                    </TwitterShareButton>
-                    <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
+const Card: React.SFC<Props> = ({ post }: Props) => (
+    <>
+        <Link href={`/mag/${post.slug}`}>
+            <a className="mag-card-link">
+                <div className="mag-card-cover-img-container">
+                    <div className="mag-card-cover-img" style={{ backgroundImage: `url("${post.thumbnailImage}")` }} />
                 </div>
-                <div className="mag-card-details">
-                    <p className="mag-card-details-category">{post.categoriesString}</p>
-                    <Link href={`/mag?id=${post.id}`} as={`/mag/${post.slug}`}>
-                        <a className="mag-card-details-link">
-                            <h2
-                                className="mag-card-details-title"
-                                dangerouslySetInnerHTML={createMarkup(post.title.rendered)}
-                            />
-                        </a>
-                    </Link>
-                    <span className="mag-card-details-date">
-                        {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
-                    </span>
-                </div>
-            </>
-        );
-    }
-}
+            </a>
+        </Link>
+        <div className="mag-card-share">
+            <FacebookShareButton
+                url={`https://skatekrak.com/mag/${post.slug}`}
+                quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
+            >
+                <FacebookIcon size={24} round />
+            </FacebookShareButton>
+            <TwitterShareButton
+                url={`https://skatekrak.com/mag/${post.slug}`}
+                title={decodeHTML(post.title.rendered)}
+                via="skatekrak"
+            >
+                <TwitterIcon size={24} round />
+            </TwitterShareButton>
+            <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
+        </div>
+        <div className="mag-card-details">
+            <p className="mag-card-details-category">{post.categoriesString}</p>
+            <Link href={`/mag?id=${post.id}`} as={`/mag/${post.slug}`}>
+                <a className="mag-card-details-link">
+                    <h2
+                        className="mag-card-details-title"
+                        dangerouslySetInnerHTML={createMarkup(post.title.rendered)}
+                    />
+                </a>
+            </Link>
+            <span className="mag-card-details-date">
+                {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
+            </span>
+        </div>
+    </>
+);
 
 export default Card;
