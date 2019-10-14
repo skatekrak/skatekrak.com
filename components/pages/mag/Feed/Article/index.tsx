6b2a4cd16d60629350bc7724dbfda964a5ead3e8
Arthur <a@skatekrak.com>
2019-10-14 13:45:09 +0100

    :chart_with_upwards_trend: Add TrackedPage on mag article

--- components/pages/mag/Feed/Article/index.tsx
+++ components/pages/mag/Feed/Article/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton } from 'react-share';
 
 import Link from 'components/Link';
+import TrackedPage from 'components/pages/TrackedPage';
 import createMarkup from 'lib/createMarkup';
 import decodeHTML from 'lib/decodeHTML';
 
@@ -22,51 +23,56 @@ const Article: React.SFC<Props> = ({ post, isLoading, sidebarNavIsOpen }: Props)
     <>
         {isLoading && <KrakLoading />}
         {post && (
-            <article id="mag-article" className={classNames({ hide: sidebarNavIsOpen })}>
-                <div id="mag-article-actions">
-                    <Link href="/mag">
-                        <a id="mag-article-back">Back to the mag</a>
-                    </Link>
-                    <div id="mag-article-share">
-                        <span id="mag-article-share-text">Share on:</span>
-                        <FacebookShareButton
-                            url={`https://skatekrak.com/mag/${post.slug}`}
-                            quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
-                        >
-                            <FacebookIcon size={24} round />
-                        </FacebookShareButton>
-                        <TwitterShareButton
-                            url={`https://skatekrak.com/mag/${post.slug}`}
-                            title={decodeHTML(post.title.rendered)}
-                            via="skatekrak"
-                        >
-                            <TwitterIcon size={24} round />
-                        </TwitterShareButton>
-                        <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
-                    </div>
-                </div>
-                <header id="mag-article-header">
-                    <p id="mag-article-category">{post.categoriesString}</p>
-                    <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title.rendered)} />
-                    <p id="mag-article-date">
-                        {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
-                    </p>
-                </header>
-                <main id="mag-article-main">
-                    {post._format_video_embed ? (
-                        <div id="mag-article-video" dangerouslySetInnerHTML={createMarkup(post._format_video_embed)} />
-                    ) : (
-                        <div id="mag-article-cover-img-container">
-                            <div
-                                id="mag-article-cover-img"
-                                style={{ backgroundImage: `url("${post.featuredImageFull}")` }}
-                            />
+            <TrackedPage name={decodeHTML(post.title.rendered)}>
+                <article id="mag-article" className={classNames({ hide: sidebarNavIsOpen })}>
+                    <div id="mag-article-actions">
+                        <Link href="/mag">
+                            <a id="mag-article-back">Back to the mag</a>
+                        </Link>
+                        <div id="mag-article-share">
+                            <span id="mag-article-share-text">Share on:</span>
+                            <FacebookShareButton
+                                url={`https://skatekrak.com/mag/${post.slug}`}
+                                quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
+                            >
+                                <FacebookIcon size={24} round />
+                            </FacebookShareButton>
+                            <TwitterShareButton
+                                url={`https://skatekrak.com/mag/${post.slug}`}
+                                title={decodeHTML(post.title.rendered)}
+                                via="skatekrak"
+                            >
+                                <TwitterIcon size={24} round />
+                            </TwitterShareButton>
+                            <ClipboardButton value={`https://skatekrak.com/mag/${post.slug}`} />
                         </div>
-                    )}
-                    <div id="mag-article-content" dangerouslySetInnerHTML={createMarkup(post.content.rendered)} />
-                    <Comment />
-                </main>
-            </article>
+                    </div>
+                    <header id="mag-article-header">
+                        <p id="mag-article-category">{post.categoriesString}</p>
+                        <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title.rendered)} />
+                        <p id="mag-article-date">
+                            {format(post.date, 'MMMM D')}, {format(post.date, 'YYYY')}
+                        </p>
+                    </header>
+                    <main id="mag-article-main">
+                        {post._format_video_embed ? (
+                            <div
+                                id="mag-article-video"
+                                dangerouslySetInnerHTML={createMarkup(post._format_video_embed)}
+                            />
+                        ) : (
+                            <div id="mag-article-cover-img-container">
+                                <div
+                                    id="mag-article-cover-img"
+                                    style={{ backgroundImage: `url("${post.featuredImageFull}")` }}
+                                />
+                            </div>
+                        )}
+                        <div id="mag-article-content" dangerouslySetInnerHTML={createMarkup(post.content.rendered)} />
+                        <Comment />
+                    </main>
+                </article>
+            </TrackedPage>
         )}
     </>
 );
