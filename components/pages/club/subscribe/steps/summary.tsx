e58318f9fc4cbc9ad1970810bde4060a913afc97
Maxime Cattet <m@skatekrak.com>
2019-10-15 10:08:14 +0200

    :recycle: Change summary to a function component

--- components/pages/club/subscribe/steps/summary.tsx
+++ components/pages/club/subscribe/steps/summary.tsx
@@ -9,80 +9,74 @@ type Props = {
     pricingMonth: string;
 };
 
-class Summary extends React.Component<Props, {}> {
-    public render() {
-        const { onNextClick, pricingQuarter, pricingMonth } = this.props;
-        const quarterFull: boolean = getConfig().publicRuntimeConfig.IS_QUARTERFULL;
-        return (
-            <div className="subscribe modal-two-col-container modal-two-col-form subscribe-summary">
-                <div className="modal-two-col-first-container modal-two-col-item-container">
-                    <h3 className="subscribe-summary-title">Monthly membership</h3>
-                    <p className="subscribe-summary-price">Starts at {pricingMonth} / month</p>
-                    <ul className="subscribe-summary-app-container">
-                        <li className="subscribe-summary-app">
-                            <img src="https://res.skatekrak.com/static/skatekrak.com/Icons/krakito.svg" alt="Krakito" />
-                        </li>
-                        <li className="subscribe-summary-app">
-                            <img
-                                src="https://res.skatekrak.com/static/skatekrak.com/Icons/krakapp.svg"
-                                alt="Krak app"
-                            />
-                        </li>
-                        <li className="subscribe-summary-app">
-                            <img
-                                src="https://res.skatekrak.com/static/skatekrak.com/Icons/kraksession.svg"
-                                alt="Krak session"
-                                id="club-monthly-main-app-kraksession"
-                            />
-                        </li>
-                    </ul>
-                    <ul className="subscribe-summary-benefits">
-                        <li className="subscribe-summary-benefit">Access to: Krakito</li>
-                        <li className="subscribe-summary-benefit">Deals on skate goods</li>
-                        <li className="subscribe-summary-benefit">Invites to secret events</li>
-                        <li className="subscribe-summary-benefit">Special features on our apps</li>
-                    </ul>
-                    <a
-                        className="button-primary subscribe-summary-cta"
-                        href="https://www.krakito.com"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        Join
-                    </a>
-                </div>
-                <div className="modal-two-col-second-container modal-two-col-item-container">
-                    <h3 className="subscribe-summary-title">Quarterly membership</h3>
-                    <p className="subscribe-summary-price">{pricingQuarter} / quarterly</p>
-                    <div className="subscribe-summary-quarter-status">
-                        {!quarterFull ? (
-                            'Become a Kraken.'
-                        ) : (
-                            <div className="subscribe-summary-quarter-full">
-                                <span>Next batch</span>
-                                <IconSoldOut />
-                                <span>{removeYear(getConfig().publicRuntimeConfig.NEXT_QUARTER_START)}</span>
-                            </div>
-                        )}
-                    </div>
-                    <ul className="subscribe-summary-benefits">
-                        <li className="subscribe-summary-benefit">
-                            <span>everything from the monthly plan +</span>
-                        </li>
-                        <li className="subscribe-summary-benefit">an exclusive deck +</li>
-                        <li className="subscribe-summary-benefit">an exclusive T-shirt +</li>
-                        <li className="subscribe-summary-benefit">the one & only KrakBox +</li>
-                    </ul>
-                    <p className="subscribe-summary-cta-text">Limited quantities available</p>
-                    <button className="button-primary subscribe-summary-cta" onClick={onNextClick}>
-                        Save your spot
-                    </button>
-                </div>
+const Summary = (props: Props) => {
+    const { onNextClick, pricingQuarter, pricingMonth } = this.props;
+    const quarterFull: boolean = getConfig().publicRuntimeConfig.IS_QUARTERFULL;
+    return (
+        <div className="subscribe modal-two-col-container modal-two-col-form subscribe-summary">
+            <div className="modal-two-col-first-container modal-two-col-item-container">
+                <h3 className="subscribe-summary-title">Monthly membership</h3>
+                <p className="subscribe-summary-price">Starts at {pricingMonth} / month</p>
+                <ul className="subscribe-summary-app-container">
+                    <li className="subscribe-summary-app">
+                        <img src="https://res.skatekrak.com/static/skatekrak.com/Icons/krakito.svg" alt="Krakito" />
+                    </li>
+                    <li className="subscribe-summary-app">
+                        <img src="https://res.skatekrak.com/static/skatekrak.com/Icons/krakapp.svg" alt="Krak app" />
+                    </li>
+                    <li className="subscribe-summary-app">
+                        <img
+                            src="https://res.skatekrak.com/static/skatekrak.com/Icons/kraksession.svg"
+                            alt="Krak session"
+                            id="club-monthly-main-app-kraksession"
+                        />
+                    </li>
+                </ul>
+                <ul className="subscribe-summary-benefits">
+                    <li className="subscribe-summary-benefit">Access to: Krakito</li>
+                    <li className="subscribe-summary-benefit">Deals on skate goods</li>
+                    <li className="subscribe-summary-benefit">Invites to secret events</li>
+                    <li className="subscribe-summary-benefit">Special features on our apps</li>
+                </ul>
+                <a
+                    className="button-primary subscribe-summary-cta"
+                    href="https://www.krakito.com"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
+                    Join
+                </a>
             </div>
-        );
-    }
-}
-
+            <div className="modal-two-col-second-container modal-two-col-item-container">
+                <h3 className="subscribe-summary-title">Quarterly membership</h3>
+                <p className="subscribe-summary-price">{pricingQuarter} / quarterly</p>
+                <div className="subscribe-summary-quarter-status">
+                    {!quarterFull ? (
+                        'Become a Kraken.'
+                    ) : (
+                        <div className="subscribe-summary-quarter-full">
+                            <span>Next batch</span>
+                            <IconSoldOut />
+                            <span>{removeYear(getConfig().publicRuntimeConfig.NEXT_QUARTER_START)}</span>
+                        </div>
+                    )}
+                </div>
+                <ul className="subscribe-summary-benefits">
+                    <li className="subscribe-summary-benefit">
+                        <span>everything from the monthly plan +</span>
+                    </li>
+                    <li className="subscribe-summary-benefit">an exclusive deck +</li>
+                    <li className="subscribe-summary-benefit">an exclusive T-shirt +</li>
+                    <li className="subscribe-summary-benefit">the one & only KrakBox +</li>
+                </ul>
+                <p className="subscribe-summary-cta-text">Limited quantities available</p>
+                <button className="button-primary subscribe-summary-cta" onClick={onNextClick}>
+                    Save your spot
+                </button>
+            </div>
+        </div>
+    );
+};
 /**
  * We assume that the date is like this:
  * October 5th 2019
