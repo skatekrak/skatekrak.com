ac4f14438e354ee5089ce37c452e34fd0162ac18
Arthur <a@skatekrak.com>
2019-10-17 15:29:54 +0100

    :sparkles: Finish Krak app page

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -4,35 +4,62 @@ import Head from 'next/head';
 import React from 'react';
 
 import Layout from 'components/Layout/Layout';
+import IconAppStore from 'components/Ui/Icons/Stores/Appstore';
+import IconPlayStore from 'components/Ui/Icons/Stores/Playstore';
 
 const AppHead = () => {
     const baseURL = getConfig().publicRuntimeConfig.WEBSITE_URL;
     return (
         <Head>
             <title>Krak App | skateboarding</title>
-            <meta name="description" content="Krak app" />
+            <meta
+                name="description"
+                content="Krak is a new way for skateboarders to share photos, videos, and spots with their friends and community. Stack clips, build your catalog, get noticed for your skills, discover new spots, and the best way to get there. By skateboarders for skateboarders."
+            />
             <meta property="og:title" content="Krak app" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={`${baseURL}/app`} />
             <meta property="og:image" content={`${baseURL}/images/og-home.jpg`} />
-            <meta property="og:description" content="Krak app" />
-            {/* TODO: Connect WP OG tags */}
+            <meta
+                property="og:description"
+                content="Krak is a new way for skateboarders to share photos, videos, and spots with their friends and community. Stack clips, build your catalog, get noticed for your skills, discover new spots, and the best way to get there. By skateboarders for skateboarders."
+            />
         </Head>
     );
 };
 
 const App: NextPage = () => (
     <Layout head={<AppHead />}>
-        <div id="app-container" className="inner-page-container">
-            <div id="app-illustration-container">L'illustration de la pocket</div>
-            <h1 id="app-title">Skateboarding culture in your pocket</h1>
-            <div id="app-links">
-                <a href="#" className="app-link">
-                    App store
-                </a>
-                <a href="#" className="app-link">
-                    Android store
-                </a>
+        <div id="app-container" className="inner-page-container container-fluid">
+            <div id="app-content-container">
+                <img
+                    src={`${getConfig().publicRuntimeConfig.WEBSITE_URL}/images/krak-app-illustration.svg`}
+                    alt=""
+                    id="app-illustration"
+                />
+                <h1 id="app-title">
+                    Skateboarding culture
+                    <br />
+                    in your pocket
+                </h1>
+                <div id="app-links">
+                    <a
+                        className="app-link"
+                        href="https://itunes.apple.com/us/app/krak/id916474561"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <IconAppStore />
+                    </a>
+                    <a
+                        className="app-link"
+                        href="https://play.google.com/store/apps/details?id=com.krak"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <IconPlayStore />
+                    </a>
+                </div>
             </div>
         </div>
     </Layout>
