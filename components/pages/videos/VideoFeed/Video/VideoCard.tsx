33d5a8b6873cd7272e3faa6524f0ebb93af04f5a
Maxime Cattet <m@skatekrak.com>
2019-10-18 15:41:47 +0200

    :recycle: Clean compo as functions

--- components/pages/videos/VideoFeed/Video/VideoCard.tsx
+++ components/pages/videos/VideoFeed/Video/VideoCard.tsx
@@ -10,28 +10,20 @@ type Props = {
     video: Video;
 };
 
-type State = {};
+const VideoCard = ({ video }: Props) => (
+    <div className="video-card">
+        <VideoPlayerCaption video={video} />
+        <VideoCardShare video={video} />
+        <div className="video-card-details">
+            <p className="video-card-details-source">
+                by {video.source.title}
+                <span className="video-card-details-date">
+                    &nbsp;- {formatDistanceToNow(parseISO(video.createdAt))}
+                </span>
+            </p>
+        </div>
+        <h2 className="video-card-title">{video.title}</h2>
+    </div>
+);
 
-class VideoCard extends React.PureComponent<Props, State> {
-    public render() {
-        const { video } = this.props;
-
-        return (
-            <div className="video-card">
-                <VideoPlayerCaption video={video} />
-                <VideoCardShare video={video} />
-                <div className="video-card-details">
-                    <p className="video-card-details-source">
-                        by {video.source.title}
-                        <span className="video-card-details-date">
-                            &nbsp;- {formatDistanceToNow(parseISO(video.createdAt))}
-                        </span>
-                    </p>
-                </div>
-                <h2 className="video-card-title">{video.title}</h2>
-            </div>
-        );
-    }
-}
-
-export default VideoCard;
+export default React.memo(VideoCard);
