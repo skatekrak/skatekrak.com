cedd330658757f4b79390de2d3736c5b3d5491d0
Maxime Cattet <m@skatekrak.com>
2019-10-22 12:41:23 +0200

    :zap: Create image element to handle retina imgsrc

---
+++ components/Ui/Image.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import createPropsGetter from 'lib/getProps';
+
+type Props = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> &
+    Partial<DefaultProps>;
+
+type DefaultProps = Readonly<typeof defaultProps>;
+
+const defaultProps = {
+    retina: false,
+};
+
+const getProps = createPropsGetter(defaultProps);
+
+const Image = (rawProps: Props) => {
+    const props = getProps(rawProps);
+
+    if (props.retina) {
+        props.srcSet = generateSrcSet(props.src);
+    }
+
+    return <img {...props} />;
+};
+
+function generateSrcSet(url: string): string {
+    const split = url.split('.');
+    const extension = split[split.length - 1];
+    return url.replace(`.${extension}`, `@2x.${extension}`) + ' 2x';
+}
+
+export default Image;
