7e73e872edd91e88e43eb34b0d4878c070252ae6
Maxime Cattet <m@skatekrak.com>
2019-10-25 20:28:53 +0200

    :fire: Remove now useless env

--- next.config.js
+++ next.config.js
@@ -17,10 +17,8 @@ module.exports = withPlugins([withBundleAnalyzer, withCSS, withStylus, withCusto
         NODE_ENV: process.env.NODE_ENV,
 
         CACHING_URL: process.env.CACHING_URL,
-        CAIROTE_URL: process.env.CAIROTE_URL,
         REDIRECT_URL: process.env.REDIRECT_URL,
         RSS_BACKEND_URL: process.env.RSS_BACKEND_URL,
-        SESTERCES_URL: process.env.SESTERCES_URL,
         KRAKMAG_URL: process.env.KRAKMAG_URL,
         TALENT_URL: process.env.TALENT_URL,
 
