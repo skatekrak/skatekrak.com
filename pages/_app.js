23e0f8a05d09819bf1f7f58f3a6d70083f490018
Arthur <a@skatekrak.com>
2019-10-28 14:28:57 +0000

    :sparkles: Refresh scroll when navigate on a new page

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,17 @@ import { Provider } from 'react-redux';
 
 import withReduxStore from 'hocs/withRedux';
 
+import RefreshScrollOnNewPage from 'components/Ui/Utils/RefreshScrollOnNewPage';
+
 class MyApp extends App {
     render() {
         const { Component, pageProps, reduxStore } = this.props;
 
         return (
             <Provider store={reduxStore}>
-                <Component {...pageProps} />
+                <RefreshScrollOnNewPage>
+                    <Component {...pageProps} />
+                </RefreshScrollOnNewPage>
             </Provider>
         );
     }
