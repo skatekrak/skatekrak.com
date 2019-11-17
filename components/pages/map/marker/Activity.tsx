b86f7dc56d60f1ccf532aa74956157795eacdaad
Arthur <a@skatekrak.com>
2019-11-17 15:02:11 +0000

    :lipstick: Improve intensity activity animation on SpotMarker

--- components/pages/map/marker/Activity.tsx
+++ components/pages/map/marker/Activity.tsx
@@ -7,8 +7,8 @@ export default ({ firing }) => (
         `}
         viewBox="0 0 48 48"
     >
-        <circle className="map-marker-activity-inner" cx="24" cy="24" r="13" />
-        <circle className="map-marker-activity-middle" cx="24" cy="24" r="18" />
-        <circle className="map-marker-activity-outter" cx="24" cy="24" r="24" />
+        <circle className="map-marker-activity-inner" opacity="0" cx="24" cy="24" r="24" />
+        <circle className="map-marker-activity-middle" opacity="0" cx="24" cy="24" r="24" />
+        <circle className="map-marker-activity-outter" opacity="0" cx="24" cy="24" r="24" />
     </svg>
 );
