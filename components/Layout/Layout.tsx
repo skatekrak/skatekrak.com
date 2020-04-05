59ac0e25c4f49e8b2d8fc582517773037fb98d06
Arthur <a@skatekrak.com>
2020-04-05 16:20:55 +0100

    :recycle: Refactor Layout into functional component and give router props to Header

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
 import getConfig from 'next/config';
 import Head from 'next/head';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import { connect, DispatchProp } from 'react-redux';
 
 import Types from 'Types';
@@ -47,46 +48,44 @@ type IComponentOwnProps = {
 
 type IComponentProps = IComponentOwnProps & IComponentStoreProps & DispatchProp;
 
-class Layout extends React.Component<IComponentProps> {
-    public componentDidMount() {
-        window.addEventListener('resize', this.setWindowsDimensions);
-        this.setWindowsDimensions();
-    }
-
-    public componentWillUnmount() {
-        window.removeEventListener('resize', this.setWindowsDimensions);
-    }
-
-    public render() {
-        const { head, children, isMobile } = this.props;
-        return (
-            <div>
-                {head ? (
-                    head
-                ) : (
-                    <Head>
-                        <title>Krak Skateboarding</title>
-                        <meta name="description" content="" />
-                        <meta property="og:title" content="Krak - Dig deeper into skateboarding" />
-                        <meta property="og:type" content="website" />
-                        <meta property="og:description" content="" />
-                        <meta property="og:url" content={getConfig().publicRuntimeConfig.WEBSITE_URL} />
-                    </Head>
-                )}
-                <div id="page-container" className={classNames({ 'scroll-container': isMobile })}>
-                    <Header />
-                    <main id="main-container" className={classNames({ 'scroll-container': !isMobile })}>
-                        {children}
-                    </main>
-                </div>
-            </div>
-        );
-    }
-
-    private setWindowsDimensions = () => {
-        this.props.dispatch(setDeviceSize(window.innerWidth));
+const Layout: React.FC<IComponentProps> = ({ head, children, isMobile, ...props }) => {
+    const setWindowsDimensions = () => {
+        props.dispatch(setDeviceSize(window.innerWidth));
     };
-}
+
+    useEffect(() => {
+        window.addEventListener('resize', setWindowsDimensions);
+        props.dispatch(setDeviceSize(window.innerWidth));
+        return () => {
+            window.removeEventListener('resize', setWindowsDimensions);
+        };
+    }, []);
+
+    const router = useRouter();
+
+    return (
+        <div>
+            {head ? (
+                head
+            ) : (
+                <Head>
+                    <title>Krak Skateboarding</title>
+                    <meta name="description" content="" />
+                    <meta property="og:title" content="Krak - Dig deeper into skateboarding" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:description" content="" />
+                    <meta property="og:url" content={getConfig().publicRuntimeConfig.WEBSITE_URL} />
+                </Head>
+            )}
+            <div id="page-container" className={classNames({ 'scroll-container': isMobile })}>
+                <Header router={router} />
+                <main id="main-container" className={classNames({ 'scroll-container': !isMobile })}>
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+};
 
 // export default Layout;
 const mapStateToProps = ({ settings }: Types.RootState) => {
