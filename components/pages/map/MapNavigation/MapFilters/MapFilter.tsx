d69cc4b9e50c0a1e322de7c809e57a88ae85112a
Maxime Cattet <maximecattet@gmx.com>
2020-06-03 21:51:24 +0200

    :recycle: Add icon props to MapFilters
    To make the MapFilter component a bit more flexible and avoid writing repetitive iteration over filters, pass the icon in the MapFilter props

--- components/pages/map/MapNavigation/MapFilters/MapFilter.tsx
+++ components/pages/map/MapNavigation/MapFilters/MapFilter.tsx
@@ -1,22 +1,15 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
 
-import DiyIcon from '../../marker/icons/Diy';
-import ParkIcon from '../../marker/icons/Park';
-import PrivateIcon from '../../marker/icons/Private';
-import RipIcon from '../../marker/icons/Rip';
-import ShopIcon from '../../marker/icons/Shop';
-import StreetIcon from '../../marker/icons/Street';
-import WipIcon from '../../marker/icons/Wip';
-
 import { FilterName } from './index';
 
 type Props = {
     name: FilterName;
+    icon: JSX.Element;
     onFilterClick: (name: FilterName) => void;
 };
 
-const MapFilter: React.FC<Props> = ({ name, onFilterClick }) => {
+const MapFilter: React.FC<Props> = ({ name, icon, onFilterClick }) => {
     const [isActive, setIsActive] = useState(true);
 
     const handleOnClick = () => {
@@ -31,13 +24,7 @@ const MapFilter: React.FC<Props> = ({ name, onFilterClick }) => {
             })}
             onClick={handleOnClick}
         >
-            {name === FilterName.STREET && <StreetIcon />}
-            {name === FilterName.PARK && <ParkIcon />}
-            {name === FilterName.DIY && <DiyIcon />}
-            {name === FilterName.PRIVATE && <PrivateIcon />}
-            {name === FilterName.SHOP && <ShopIcon />}
-            {name === FilterName.WIP && <WipIcon />}
-            {name === FilterName.RIP && <RipIcon />}
+            {icon}
         </button>
     );
 };
--- components/pages/map/MapNavigation/MapFilters/index.tsx
+++ components/pages/map/MapNavigation/MapFilters/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import MapFilter from './MapFilter';
 
+import DiyIcon from 'components/pages/map/marker/icons/Diy';
+import ParkIcon from 'components/pages/map/marker/icons/Park';
+import PrivateIcon from 'components/pages/map/marker/icons/Private';
+import RipIcon from 'components/pages/map/marker/icons/Rip';
+import ShopIcon from 'components/pages/map/marker/icons/Shop';
+import StreetIcon from 'components/pages/map/marker/icons/Street';
+import WipIcon from 'components/pages/map/marker/icons/Wip';
+
 export enum FilterName {
     STREET = 'street',
     PARK = 'park',
@@ -19,13 +27,13 @@ const index = () => {
 
     return (
         <div id="map-navigation-filters">
-            <MapFilter name={FilterName.STREET} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.PARK} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.DIY} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.PRIVATE} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.SHOP} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.WIP} onFilterClick={onFilterClick} />
-            <MapFilter name={FilterName.RIP} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.STREET} icon={<StreetIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.PARK} icon={<ParkIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.DIY} icon={<DiyIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.PRIVATE} icon={<PrivateIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.SHOP} icon={<ShopIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.WIP} icon={<WipIcon />} onFilterClick={onFilterClick} />
+            <MapFilter name={FilterName.RIP} icon={<RipIcon />} onFilterClick={onFilterClick} />
         </div>
     );
 };
