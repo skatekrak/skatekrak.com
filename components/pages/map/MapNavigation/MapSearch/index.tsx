db1b64390246458a91b6ec778319b94f40a4b829
Maxime Cattet <maximecattet@gmx.com>
2020-07-14 10:28:25 +0100

    Replace swr by react-query
    react-query has nicer handling of loading state, which avoid the search being stuck in a loading state

--- components/pages/map/MapNavigation/MapSearch/index.tsx
+++ components/pages/map/MapNavigation/MapSearch/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import useConstant from 'use-constant';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
-import useSWR from 'swr';
+import { useQuery } from 'react-query';
 
-import { querySpotsSearch, QuerySearchSpotsParam } from 'lib/carrelageClient';
+import { querySpotsSearch } from 'lib/carrelageClient';
 import MapSearchBar from './MapSearchBar';
 import MapSearchResults from './MapSearchResults/MapSearchResults';
 
@@ -11,15 +11,15 @@ const index = () => {
     const [searchValue, setSearchValue] = useState('');
 
     const debouncedSearch = useConstant(() => AwesomeDebouncePromise(querySpotsSearch, 200));
-    const { data, isValidating } = useSWR(
-        searchValue.length === 0 ? null : ['spots-search', searchValue],
-        (key, value) => {
-            const param: QuerySearchSpotsParam = { query: value };
-            return debouncedSearch(param);
+    const { isLoading, data } = useQuery(
+        ['search-spots', { query: searchValue }],
+        (key, { query }) => {
+            if (!query) {
+                return null;
+            }
+            return debouncedSearch({ query });
         },
-        {
-            revalidateOnFocus: false,
-        },
+        { refetchOnWindowFocus: false },
     );
 
     const handleSearchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,7 +37,7 @@ const index = () => {
                 handleSearchChange={handleSearchChange}
                 clearSearchValue={clearSearchValue}
             />
-            {searchValue.length !== 0 && <MapSearchResults spots={data ?? []} loading={isValidating} />}
+            {searchValue.length !== 0 && <MapSearchResults spots={data ?? []} loading={isLoading} />}
         </div>
     );
 };
