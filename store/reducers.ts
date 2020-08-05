029afdf7ffd4967bd86d14e626301c2ce9acd1fa
Maxime Cattet <maximecattet@gmx.com>
2020-08-05 09:54:11 +0200

    :fire: Remove dead code and libraries
    Remove dead code that were not used anymore, as well as attached libraries to that dead code or just unused libraries

--- store/reducers.ts
+++ store/reducers.ts
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 
 import feed from './feed/reducers';
-import form from './form/reducers';
 import payment from './payment/reducers';
 import settings from './settings/reducers';
 import map from './map/reducers';
 
 export default () =>
     combineReducers({
-        form,
         payment,
         settings,
         news: feed(),
