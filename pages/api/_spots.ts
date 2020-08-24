066ae1b1e4e0402dec41a589c6533bdc35a1eee7
Maxime Cattet <maximecattet@gmx.com>
2020-08-24 20:59:28 +0200

    :sparkles: Create API routes to get custom maps info
    With some demo spots

---
+++ pages/api/_spots.ts
@@ -0,0 +1,65 @@
+export default [
+    {
+        id: 'volcom',
+        name: 'Volcom Skate Map',
+        subtitle: 'The stoned map',
+        about:
+            'Phasellus pellentesque mauris tempor aliquam tristique. Suspendisse ac leo venenatis, pharetra nunc vehicula, facilisis erat. Mauris quis libero in ex volutpat facilisis. Morbi vitae risus at augue porttitor ornare. Nullam ultricies massa orci, sed facilisis elit aliquet id. Pellentesque enim nisi, gravida ac ante nec, volutpat porttitor sem. Etiam ac sapien viverra velit sollicitudin vehicula.',
+        spots: [
+            {
+                id: '5dbdb307b0d22d001cf7a339',
+                status: 'active',
+                name: 'Mairie du 4e',
+                type: 'street',
+                location: {
+                    streetNumber: '25',
+                    streetName: 'Rue de Rivoli',
+                    city: 'Paris',
+                    country: 'France',
+                    longitude: 2.3559096519579725,
+                    latitude: 48.85642825351378,
+                },
+                tags: [],
+                mediasStat: {
+                    all: 0,
+                },
+            },
+            {
+                id: '5dbdb06f222632001c08bfa1',
+                status: 'active',
+                name: 'Plaque Rey Marcel',
+                type: 'street',
+                location: {
+                    streetNumber: '1',
+                    streetName: 'Parvis Notre Dame - Place Jean-Paul II',
+                    city: 'Paris',
+                    country: 'France',
+                    longitude: 2.348962404212557,
+                    latitude: 48.853592785012594,
+                },
+                tags: [],
+                mediasStat: {
+                    all: 0,
+                },
+            },
+            {
+                id: '5d8a3d8540aca9001be6fff0',
+                status: 'active',
+                name: 'Quai d’orsay',
+                type: 'street',
+                location: {
+                    streetNumber: '9',
+                    streetName: 'Quai Anatole France',
+                    city: 'Paris',
+                    country: 'France',
+                    longitude: 2.324426650060292,
+                    latitude: 48.861173743756424,
+                },
+                tags: [],
+                mediasStat: {
+                    all: 0,
+                },
+            },
+        ],
+    },
+];
