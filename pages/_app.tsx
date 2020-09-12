2e7328d3f9b74dc166b2e7aac3671ff7927f4a2f
Maxime Cattet <maximecattet@gmx.com>
2020-09-12 12:41:45 +0200

    :sparkles: Add bugsnag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginReact from '@bugsnag/plugin-react';
+
+Bugsnag.start({
+    apiKey: '4682b68bd8f242f45afba6142adba586',
+    plugins: [new BugsnagPluginReact()],
+});
 
 import { wrapper } from 'store';
 import Head from 'next/head';
 
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
+
 const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-    <>
+    <ErrorBoundary>
         <Head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="with-device-with, initial-scale=1" />
         </Head>
         <Component {...pageProps} />
-    </>
+    </ErrorBoundary>
 );
 
 export default wrapper.withRedux(WrappedApp);
