3864afdf4ce07c3602cc0792f4c90a85714039d9
Maxime Cattet <maximecattet@gmx.com>
2020-10-27 23:28:04 +0100

    :fire: Remove jenkem from custom maps

--- data/_spots.ts
+++ data/_spots.ts
@@ -1,14 +1,5 @@
 export default [
     {
-        id: 'jenkem',
-        name: 'JENKEM',
-        subtitle: 'Evolution of...',
-        edito: 'There are still many more spots worth chronicling for our ‘Evolution of…’ series.',
-        about:
-            'In an effort to educate ourselves and any other skateboarders, we decided to visit some of the most iconic spots with some of the city’s foremost skate historians and ask them to walk us through the timelines of these architectural blessings.\nWhat we came up with is a new series on the “Evolution” of New York City’s [*sic*] skate spots.',
-        spots: require('data/evolutionof.json'),
-    },
-    {
         id: 'spohnranch',
         name: 'Spohn Ranch',
         subtitle: 'Spohn Ranch Skateparks',
