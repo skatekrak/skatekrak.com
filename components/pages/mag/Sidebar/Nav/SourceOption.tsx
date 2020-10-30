6ad90ffae4fe164bfee60bd67a8a1a863fe94aca
Maxime Cattet <maximecattet@gmx.com>
2020-10-30 15:51:25 +0100

    :recycle: Move SourceOption to function component

--- components/pages/mag/Sidebar/Nav/SourceOption.tsx
+++ components/pages/mag/Sidebar/Nav/SourceOption.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { SpinnerCircle } from 'components/Ui/Icons/Spinners';
 import Analytics from 'lib/analytics';
@@ -8,62 +8,42 @@ import { FilterState } from 'lib/FilterState';
 import { Source } from 'rss-feed';
 import { toggleFilter } from 'store/feed/actions';
 
-type Props = {
+type SourceOptionProps = {
     source: Source;
     state: FilterState;
-    dispatch: (fct: any) => void;
 };
 
-type State = {
-    isActive: boolean;
-    isLoading: boolean;
-};
+const SourceOption = ({ source, state }: SourceOptionProps) => {
+    const isLoading = state === FilterState.LOADING_TO_SELECTED || state === FilterState.LOADING_TO_UNSELECTED;
+    const isActive = state === FilterState.SELECTED || state !== FilterState.UNSELECTED;
 
-class SourceOption extends React.PureComponent<Props, State> {
-    public static getDerivedStateFromProps(nextProps: Readonly<Props>): State {
-        return {
-            isActive: nextProps.state === FilterState.SELECTED || nextProps.state !== FilterState.UNSELECTED,
-            isLoading:
-                nextProps.state === FilterState.LOADING_TO_SELECTED ||
-                nextProps.state === FilterState.LOADING_TO_UNSELECTED,
-        };
-    }
+    const dispatch = useDispatch();
 
-    public state: State = {
-        isActive: true,
-        isLoading: false,
-    };
-
-    public render() {
-        const { isActive, isLoading } = this.state;
-        const { source } = this.props;
-
-        return (
-            <li
-                className={classNames('feed-sidebar-nav-option feed-sidebar-nav-option-without-logo', {
-                    'feed-sidebar-nav-option--active': isActive,
-                })}
-            >
-                <label
-                    htmlFor={`input-${source.id}`}
-                    className="feed-sidebar-nav-option-label"
-                    onClick={this.handleSourceOptionClick}
-                >
-                    <span className="feed-sidebar-nav-option-name">{source.label}</span>
-                    {isLoading && <SpinnerCircle />}
-                </label>
-            </li>
-        );
-    }
-
-    private handleSourceOptionClick = () => {
-        if (this.props.state === FilterState.SELECTED) {
-            Analytics.trackEvent('Click', 'Filter_Unselect', { name: this.props.source.label, value: 1 });
-        } else if (this.props.state === FilterState.UNSELECTED) {
-            Analytics.trackEvent('Click', 'Filter_Select', { name: this.props.source.label, value: 1 });
+    const handleSourceOptionClick = () => {
+        if (state === FilterState.SELECTED) {
+            Analytics.trackEvent('Click', 'Filter_Unselect', { name: source.label, value: 1 });
+        } else if (state === FilterState.UNSELECTED) {
+            Analytics.trackEvent('Click', 'Filter_Select', { name: source.label, value: 1 });
         }
-        this.props.dispatch(toggleFilter(this.props.source));
+        dispatch(toggleFilter(source));
     };
-}
 
-export default connect()(SourceOption);
+    return (
+        <li
+            className={classNames('feed-sidebar-nav-option feed-sidebar-nav-option-without-logo', {
+                'feed-sidebar-nav-option--active': isActive,
+            })}
+        >
+            <label
+                htmlFor={`input-${source.id}`}
+                className="feed-sidebar-nav-option-label"
+                onClick={handleSourceOptionClick}
+            >
+                <span className="feed-sidebar-nav-option-name">{source.label}</span>
+                {isLoading && <SpinnerCircle />}
+            </label>
+        </li>
+    );
+};
+
+export default React.memo(SourceOption);
