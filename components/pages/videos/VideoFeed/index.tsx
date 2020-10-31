18abc0e34e50c43e63e596defa8e6bd2c5642078
Maxime Cattet <maximecattet@gmx.com>
2020-10-31 22:01:24 +0100

    :recycle: Move VideoFeed to Function component

--- components/pages/videos/VideoFeed/index.tsx
+++ components/pages/videos/VideoFeed/index.tsx
@@ -1,155 +1,130 @@
 import axios from 'axios';
 import classNames from 'classnames';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
-import { connect } from 'react-redux';
-
-import Types from 'Types';
+import { useDispatch, useSelector } from 'react-redux';
 
 import TrackedPage from 'components/pages/TrackedPage';
 import FeaturedVideo from 'components/pages/videos/VideoFeed/Video/FeaturedVideo';
 import VideoCard from 'components/pages/videos/VideoFeed/Video/VideoCard';
 import NoContent from 'components/Ui/Feed/NoContent';
 import { KrakLoading } from 'components/Ui/Icons/Spinners';
-import { FilterState } from 'lib/FilterState';
 import ScrollHelper from 'lib/ScrollHelper';
-import { Source, Video } from 'rss-feed';
 import { feedEndRefresh } from 'store/feed/actions';
-import { State as VideoState } from 'store/feed/reducers';
+import { getFilters } from 'store/feed/reducers';
+import { RootState } from 'store/reducers';
+import { Video } from 'rss-feed';
 
-type Props = {
-    video: VideoState;
-    dispatch: (fct: any) => void;
+type VideoFeedProps = {
     sidebarNavIsOpen: boolean;
 };
 
-type State = {
-    isLoading: boolean;
-    hasMore: boolean;
-    featuredVideo?: Video;
-    displayedVideos: Video[];
-};
+const VideoFeed = ({ sidebarNavIsOpen }: VideoFeedProps) => {
+    const dispatch = useDispatch();
+    const sources = useSelector((state: RootState) => state.video.sources);
+    const search = useSelector((state: RootState) => state.video.search);
+    const feedNeedRefresh = useSelector((state: RootState) => state.video.feedNeedRefresh);
 
-class VideoFeed extends React.Component<Props, State> {
-    public state: State = {
-        isLoading: false,
-        hasMore: true,
-        displayedVideos: [],
-    };
+    const [isLoading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const [featuredVideo, setFeaturedVideo] = useState<Video>();
+    const [displayedVideos, setDisplayedVideos] = useState<Video[]>([]);
 
-    public async componentDidMount() {
-        const req: Promise<any> = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/featured`);
-        const res = await req;
-        if (res.data) {
-            const data: Video[] = res.data;
-            this.getFeaturedVideo(data);
-        }
-    }
+    const loadMore = useCallback(
+        async (page: number) => {
+            try {
+                setLoading(true);
 
-    public async componentDidUpdate(_prevProps: Props, prevState: State) {
-        if (this.props.video.feedNeedRefresh && !this.state.isLoading) {
-            this.setState({ displayedVideos: [], hasMore: false });
-            await this.loadMore(1);
-        }
-    }
-
-    public render() {
-        const { isLoading, hasMore, featuredVideo, displayedVideos } = this.state;
-        return (
-            <div id="videos-feed-container">
-                {featuredVideo && (
-                    <div id="videos-feed-header" className="row">
-                        <div className="col-xs-12">
-                            <FeaturedVideo video={featuredVideo} />
-                        </div>
-                    </div>
-                )}
-                <TrackedPage name={`Videos/${Math.ceil(displayedVideos.length / 20)}`} initial={false} />
-                <InfiniteScroll
-                    key={`infinite-need-refresh-${this.props.video.feedNeedRefresh}`}
-                    pageStart={1}
-                    initialLoad={false}
-                    loadMore={this.loadMore}
-                    hasMore={!isLoading && hasMore}
-                    getScrollParent={this.getScrollContainer}
-                    useWindow={false}
-                >
-                    <div className={classNames('row', { hide: this.props.sidebarNavIsOpen })}>
-                        {displayedVideos.map((video, index) => (
-                            <div key={index} className="video-card-container col-xs-12 col-sm-6 col-lg-4">
-                                <VideoCard video={video} />
-                            </div>
-                        ))}
-                        {displayedVideos.length === 0 && !isLoading && (
-                            <NoContent title="No video to display" desc="Select some channels to be back in the loop" />
-                        )}
-                        {isLoading && <KrakLoading />}
-                        {displayedVideos.length > 0 && !hasMore && (
-                            <NoContent title="No more video" desc="Select other channels if you're still hungry" />
-                        )}
-                    </div>
-                </InfiniteScroll>
-            </div>
-        );
-    }
-
-    private getScrollContainer = () => {
-        return ScrollHelper.getScrollContainer();
-    };
-
-    private loadMore = async (page: number) => {
-        try {
-            this.setState({ isLoading: true });
-
-            const filters = this.getFilters(this.props.video.sources);
-            let req: Promise<any>;
-            if (filters.length === 0) {
-                req = Promise.resolve();
-            } else {
-                if (this.props.video.search) {
-                    req = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/search`, {
-                        params: { page, filters, query: this.props.video.search },
-                    });
+                const filters = getFilters(sources);
+                let req: Promise<any>;
+                if (filters.length === 0) {
+                    req = Promise.resolve();
                 } else {
-                    req = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/`, {
-                        params: { page, filters },
-                    });
+                    if (search) {
+                        req = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/search`, {
+                            params: { page, filters, query: search },
+                        });
+                    } else {
+                        req = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/`, {
+                            params: { page, filters },
+                        });
+                    }
                 }
+                const res = await req;
+                if (res.data) {
+                    const data: Video[] = res.data;
+                    setDisplayedVideos((videos) => videos.concat(data));
+                    setHasMore(data.length >= 20);
+                }
+            } catch (err) {
+                //
+            } finally {
+                dispatch(feedEndRefresh());
+                setLoading(false);
             }
+        },
+        [sources, dispatch, search],
+    );
+
+    useEffect(() => {
+        (async () => {
+            const req: Promise<any> = axios.get(`${process.env.NEXT_PUBLIC_RSS_BACKEND_URL}/videos/featured`);
             const res = await req;
             if (res.data) {
                 const data: Video[] = res.data;
-                const displayedVideos = this.state.displayedVideos;
-                this.setState({
-                    displayedVideos: displayedVideos.concat(data),
-                    hasMore: data.length >= 20,
-                });
+                if (data.length > 0) {
+                    setFeaturedVideo(data[0]);
+                }
             }
-        } catch (err) {
-            //
-        } finally {
-            this.props.dispatch(feedEndRefresh());
-            this.setState({ isLoading: false });
-        }
-    };
+        })();
+    }, []);
 
-    private getFilters(sources: Map<Source, FilterState>): string[] {
-        const arr: string[] = [];
-        for (const entry of sources.entries()) {
-            if (entry[1] === FilterState.LOADING_TO_SELECTED || entry[1] === FilterState.SELECTED) {
-                arr.push(entry[0].id);
+    useEffect(() => {
+        (async () => {
+            if (feedNeedRefresh && !isLoading) {
+                setDisplayedVideos([]);
+                setHasMore(false);
+                await loadMore(1);
             }
-        }
-        return arr;
-    }
+        })();
+    }, [feedNeedRefresh, isLoading, loadMore]);
 
-    private getFeaturedVideo = (featuredVideos: Video[]) => {
-        if (featuredVideos.length > 0) {
-            this.setState({ featuredVideo: featuredVideos[0] });
-        }
-    };
-}
+    return (
+        <div id="videos-feed-container">
+            {featuredVideo && (
+                <div id="videos-feed-header" className="row">
+                    <div className="col-xs-12">
+                        <FeaturedVideo video={featuredVideo} />
+                    </div>
+                </div>
+            )}
+            <TrackedPage name={`Videos/${Math.ceil(displayedVideos.length / 20)}`} initial={false} />
+            <InfiniteScroll
+                key={`infinite-need-refresh-${feedNeedRefresh}`}
+                pageStart={1}
+                initialLoad={false}
+                loadMore={loadMore}
+                hasMore={!isLoading && hasMore}
+                getScrollParent={ScrollHelper.getScrollContainer}
+                useWindow={false}
+            >
+                <div className={classNames('row', { hide: sidebarNavIsOpen })}>
+                    {displayedVideos.map((video, index) => (
+                        <div key={index} className="video-card-container col-xs-12 col-sm-6 col-lg-4">
+                            <VideoCard video={video} />
+                        </div>
+                    ))}
+                    {displayedVideos.length === 0 && !isLoading && (
+                        <NoContent title="No video to display" desc="Select some channels to be back in the loop" />
+                    )}
+                    {isLoading && <KrakLoading />}
+                    {displayedVideos.length > 0 && !hasMore && (
+                        <NoContent title="No more video" desc="Select other channels if you're still hungry" />
+                    )}
+                </div>
+            </InfiniteScroll>
+        </div>
+    );
+};
 
-export default connect(({ video }: Types.RootState) => ({
-    video,
-}))(VideoFeed);
+export default VideoFeed;
