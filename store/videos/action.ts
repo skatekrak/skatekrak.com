44f7c73b8e864aafefbfe4da50a9d0cb47b66e10
Maxime Cattet <maximecattet@gmx.com>
2020-11-08 13:49:31 +0100

    :sparkles: Push videos search to URL query

--- store/videos/action.ts
+++ store/videos/action.ts
@@ -5,5 +5,5 @@ import { TOGGLE_VIDEOS_SOURCE, SELECT_VIDEOS_SOURCES, SET_VIDEOS_SEARCH, RESET_V
 
 export const toggleVideosSource = (source: Source) => action(TOGGLE_VIDEOS_SOURCE, source);
 export const selectVideosSources = (sources: Source[]) => action(SELECT_VIDEOS_SOURCES, sources);
-export const setVideosSearch = (query: string) => action(SET_VIDEOS_SEARCH, query);
+export const setVideosSearch = (query: string) => action(SET_VIDEOS_SEARCH, query, { pushToUrl: { query } });
 export const resetVideos = () => action(RESET_VIDEOS);
