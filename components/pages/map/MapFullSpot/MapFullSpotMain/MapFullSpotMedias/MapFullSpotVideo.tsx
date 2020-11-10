d86819d122bdf98207354ea21400c4fb5a446bdc
Maxime Cattet <maximecattet@gmx.com>
2020-11-10 12:29:27 +0100

    :sparkles: Make sure only 1 video is playing

--- components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotMedias/MapFullSpotVideo.tsx
+++ components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotMedias/MapFullSpotVideo.tsx
@@ -1,23 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 
 import { Media } from 'lib/carrelageClient';
 import VideoPlayer from 'components/Ui/Player/VideoPlayer';
 import MapFullSpotMediaOverlay from './MapFullSpotMediaOverlay';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from 'store/reducers';
+import { setVideoPlaying } from 'store/map/actions';
+import ReactPlayer from 'react-player';
 
 export type MapFullSpotVideoProps = {
     media: Media;
 };
 
 const MapFullSpotVideo: React.FC<MapFullSpotVideoProps> = ({ media }) => {
-    const [isPlaying, setPlaying] = useState(false);
+    const dispatch = useDispatch();
+    const videoPlayingId = useSelector((state: RootState) => state.map.videoPlayingId);
+    const isPlaying = useMemo(() => videoPlayingId === media.id, [videoPlayingId, media.id]);
+    const playerRef = useRef<ReactPlayer>(null);
+
     const onReady = () => {
-        setPlaying(true);
+        dispatch(setVideoPlaying(media.id));
     };
+
+    useEffect(() => {
+        if (!isPlaying && playerRef.current) {
+            console.log('reset preview');
+            playerRef.current.showPreview();
+        }
+    }, [isPlaying]);
 
     return (
         <div key={media.id} className="map-full-spot-popup-main-media-container">
             {media.video && (
                 <VideoPlayer
+                    ref={playerRef}
                     playing={isPlaying}
                     onReady={onReady}
                     url={media.video.jpg}
@@ -26,7 +42,7 @@ const MapFullSpotVideo: React.FC<MapFullSpotVideoProps> = ({ media }) => {
                     controls
                 />
             )}
-            <MapFullSpotMediaOverlay media={media} />
+            {!isPlaying && <MapFullSpotMediaOverlay media={media} />}
         </div>
     );
 };
