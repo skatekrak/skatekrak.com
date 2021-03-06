bcbae2af49e703b71442224a61e11759ca440e55
Maxime Cattet <maximecattet@gmx.com>
2021-03-06 12:00:36 +0100

    :sparkles: Add barcelona city map

--- data/cities/_cities.ts
+++ data/cities/_cities.ts
@@ -14,6 +14,18 @@ Paris is a paradise for skateboarders! From its concrete sidewalks perfect to go
             [2.4308845217189514, 48.9121314930525],
         ],
     },
+    {
+        id: 'barcelona',
+        name: 'Barcelona',
+        edito: 'Euro skate capital',
+        subtitle: 'Macbalife!',
+        about:
+            "You can netflix n chill at Macba all day or you can go discover the crazy amount of piece of art the architectures have created in this spanish playground! It's also a wild place for the party night life..",
+        bounds: [
+            [2.0576750759531706, 41.32263417922036],
+            [2.279536643486864, 41.4467123861632],
+        ],
+    },
 ];
 
 export default cities;
