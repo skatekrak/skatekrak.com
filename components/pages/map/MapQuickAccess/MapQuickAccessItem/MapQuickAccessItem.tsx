54babc29353e546660acd3e30c67e5225f5d6bec
Maxime Cattet <maximecattet@gmx.com>
2021-06-22 15:07:18 +0200

    :fire: Remove copie

--- components/pages/map/MapQuickAccess/MapQuickAccessItem/MapQuickAccessItem.tsx
+++ components/pages/map/MapQuickAccess/MapQuickAccessItem/MapQuickAccessItem.tsx
@@ -55,9 +55,9 @@ function getSrcSet(id: string, isQuickAccessMap: boolean): string {
     }
 
     return `
-        /images/map/cities/${id}.png 1x,
-        /images/map/cities/${id}@2x.png 2x,
-        /images/map/cities/${id}@3x.png 3x
+        /images/map/cities/${id}.jpg 1x,
+        /images/map/cities/${id}@2x.jpg 2x,
+        /images/map/cities/${id}@3x.jpg 3x
     `;
 }
 
