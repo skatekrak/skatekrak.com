8e8eddf75356ee26b3a98a3d66d08be6862754b2
Maxime Cattet <maximecattet@gmx.com>
2021-06-29 12:36:11 +0200

    :bento: Change order of cities

--- data/cities/_cities.ts
+++ data/cities/_cities.ts
@@ -2,52 +2,25 @@ import { City } from 'map';
 
 const cities: City[] = [
     {
-        id: 'paris',
-        name: 'Paris',
-        edito: 'The city of Love!',
-        subtitle: 'We love Paris sidewalks!',
-        about: `
-Paris is a paradise for skateboarders! From its concrete sidewalks perfect to go get your baguette in the morning to all these iconic spots still receiving their fair amount of destruction nowadays, Paris is the place to be!
-        `,
+        id: 'shanghai',
+        name: 'Shanghai',
+        edito: '',
+        subtitle: '',
+        about: '',
         bounds: [
-            [2.2548510078733557, 48.810511704540005],
-            [2.4308845217189514, 48.9121314930525],
+            [121.41132742324771, 31.179231619930576],
+            [121.56222044527658, 31.274483881128248],
         ],
     },
     {
-        id: 'barcelona',
-        name: 'Barcelona',
-        edito: 'Euro skate capital',
-        subtitle: 'Macbalife!',
-        about:
-            "You can netflix n chill at Macba all day or you can go discover the crazy amount of piece of art the architectures have created in this spanish playground! It's also a wild place for the party night life..",
+        id: 'singapore',
+        name: 'Singapore',
+        edito: '',
+        subtitle: '',
+        about: '',
         bounds: [
-            [2.0576750759531706, 41.32263417922036],
-            [2.279536643486864, 41.4467123861632],
-        ],
-    },
-    {
-        id: 'lyon',
-        name: 'Lyon',
-        edito: 'Where this whole Krak idea started!',
-        subtitle: 'HDV everyday, all day!',
-        about:
-            "Lyon is one of the major city in the french skate scene. Lots of legends came to film with Mr Fred Mortagne 20 years ago and made all these spots worldwide famous! You can still frequently meet pros passing by on the spots. Add to that the skateparks, the incredible and recently renewed HDV Plaza spot! You can't ask any better than this to have fun on your board!",
-        bounds: [
-            [4.787438796352205, 45.738004310175],
-            [4.883625037813516, 45.78802185177571],
-        ],
-    },
-    {
-        id: 'nyc',
-        name: 'New York',
-        edito: 'The Big Apple',
-        subtitle: 'The City of Lights!',
-        about:
-            "New York City has always been a huge part of the american and worldwide history! With so many typical spots you can tell it's New York, it has a charme you can't deny if you like urban street skating! Thank you Bobby Puelo, Tyshawn Jones, and all those in between! ",
-        bounds: [
-            [-74.07686383082431, 40.69836309341969],
-            [-73.8844913479055, 40.80698836737918],
+            [103.79195666582484, 1.261332366626064],
+            [103.89591805260989, 1.3380576970246523],
         ],
     },
     {
@@ -73,6 +46,77 @@ Paris is a paradise for skateboarders! From its concrete sidewalks perfect to go
         ],
     },
     {
+        id: 'lyon',
+        name: 'Lyon',
+        edito: 'Where this whole Krak idea started!',
+        subtitle: 'HDV everyday, all day!',
+        about:
+            "Lyon is one of the major city in the french skate scene. Lots of legends came to film with Mr Fred Mortagne 20 years ago and made all these spots worldwide famous! You can still frequently meet pros passing by on the spots. Add to that the skateparks, the incredible and recently renewed HDV Plaza spot! You can't ask any better than this to have fun on your board!",
+        bounds: [
+            [4.787438796352205, 45.738004310175],
+            [4.883625037813516, 45.78802185177571],
+        ],
+    },
+    {
+        id: 'paris',
+        name: 'Paris',
+        edito: 'The city of Love!',
+        subtitle: 'We love Paris sidewalks!',
+        about: `
+Paris is a paradise for skateboarders! From its concrete sidewalks perfect to go get your baguette in the morning to all these iconic spots still receiving their fair amount of destruction nowadays, Paris is the place to be!
+        `,
+        bounds: [
+            [2.2548510078733557, 48.810511704540005],
+            [2.4308845217189514, 48.9121314930525],
+        ],
+    },
+    {
+        id: 'london',
+        name: 'London',
+        edito: '',
+        subtitle: '',
+        about: '',
+        bounds: [
+            [-0.23693493552042355, 51.457159797428545],
+            [-0.018581664037128576, 51.557486774307336],
+        ],
+    },
+    {
+        id: 'barcelona',
+        name: 'Barcelona',
+        edito: 'Euro skate capital',
+        subtitle: 'Macbalife!',
+        about:
+            "You can netflix n chill at Macba all day or you can go discover the crazy amount of piece of art the architectures have created in this spanish playground! It's also a wild place for the party night life..",
+        bounds: [
+            [2.0576750759531706, 41.32263417922036],
+            [2.279536643486864, 41.4467123861632],
+        ],
+    },
+    {
+        id: 'brooklyn',
+        name: 'Brooklyn',
+        edito: '',
+        subtitle: '',
+        about: '',
+        bounds: [
+            [-74.0619176038734, 40.617289129755534],
+            [-73.84356433239117, 40.73953236875826],
+        ],
+    },
+    {
+        id: 'nyc',
+        name: 'New York',
+        edito: 'The Big Apple',
+        subtitle: 'The City of Lights!',
+        about:
+            "New York City has always been a huge part of the american and worldwide history! With so many typical spots you can tell it's New York, it has a charme you can't deny if you like urban street skating! Thank you Bobby Puelo, Tyshawn Jones, and all those in between! ",
+        bounds: [
+            [-74.07686383082431, 40.69836309341969],
+            [-73.8844913479055, 40.80698836737918],
+        ],
+    },
+    {
         id: 'la',
         name: 'Los Angeles',
         edito: '',
@@ -92,50 +136,6 @@ Paris is a paradise for skateboarders! From its concrete sidewalks perfect to go
         bounds: [
             [-122.52026422301677, 37.73189107529478],
             [-122.36978418038728, 37.817769558834755],
-        ],
-    },
-    {
-        id: 'brooklyn',
-        name: 'Brooklyn',
-        edito: '',
-        subtitle: '',
-        about: '',
-        bounds: [
-            [-74.0619176038734, 40.617289129755534],
-            [-73.84356433239117, 40.73953236875826],
-        ],
-    },
-    {
-        id: 'london',
-        name: 'London',
-        edito: '',
-        subtitle: '',
-        about: '',
-        bounds: [
-            [-0.23693493552042355, 51.457159797428545],
-            [-0.018581664037128576, 51.557486774307336],
-        ],
-    },
-    {
-        id: 'shanghai',
-        name: 'Shanghai',
-        edito: '',
-        subtitle: '',
-        about: '',
-        bounds: [
-            [121.41132742324771, 31.179231619930576],
-            [121.56222044527658, 31.274483881128248],
-        ],
-    },
-    {
-        id: 'singapore',
-        name: 'Singapore',
-        edito: '',
-        subtitle: '',
-        about: '',
-        bounds: [
-            [103.79195666582484, 1.261332366626064],
-            [103.89591805260989, 1.3380576970246523],
         ],
     },
 ];
