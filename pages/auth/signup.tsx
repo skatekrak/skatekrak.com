ee509b449cb563e3d4e05650a8db7724e672674c
Maxime Cattet <maximecattet@gmx.com>
2022-03-09 10:26:48 +0100

    feat(signup): Connect Feudartifice on Signup
    Also move schema validation to Yup

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Formik, Field, FormikErrors, FormikHelpers } from 'formik';
+import { Formik, Field, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import * as _ from 'lodash';
 
 import Layout from 'components/Layout';
 import ButtonPrimary from 'components/Ui/Button/ButtonPrimary/ButtonPrimary';
 import Emoji from 'components/Ui/Icons/Emoji';
 
 import * as S from 'components/pages/auth/Auth.styled';
+import Feudartifice from 'shared/feudartifice';
+import { CarrelageAPIError } from 'shared/feudartifice/types';
 
 type SignupFormValues = {
     username: string;
@@ -16,19 +20,41 @@ type SignupFormValues = {
     confirmPassword: string;
 };
 
+const SignupFormSchema = Yup.object().shape({
+    username: Yup.string()
+        .min(1, 'Username must be more than one letter')
+        .max(15, 'Username cannot be more than 15 letters')
+        .matches(/^[a-z0-9_]+$/, {
+            message: "Username should only contains alphanumeric or '_' characters",
+            excludeEmptyString: true,
+        })
+        .required()
+        .default(''),
+    email: Yup.string().email("This isn't a valid email").required().default(''),
+    password: Yup.string().min(6, 'Password must be at least 6 characters long').required().default(''),
+    confirmPassword: Yup.string().required().default(''), // check equality
+});
+
 const Signup: NextPage = () => {
     const router = useRouter();
 
-    const initialValues: SignupFormValues = {
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-    };
-
     const onSubmit = async (values: SignupFormValues, helpers: FormikHelpers<SignupFormValues>) => {
-        console.log('value: ', values);
-        router.push('/auth/subscribe');
+        try {
+            await Feudartifice.auth.signup({
+                username: values.username,
+                email: values.email,
+                password: values.password,
+            });
+
+            // TODO: query to get currency of user
+            const { url } = await Feudartifice.payments.getCheckoutSession();
+            router.push(url);
+        } catch (err) {
+            if (err.response) {
+                const error = err.response as CarrelageAPIError;
+                helpers.setFieldError('username', error.data.message);
+            }
+        }
     };
 
     return (
@@ -37,7 +63,11 @@ const Signup: NextPage = () => {
                 <S.AuthUniqueColumnPage>
                     <S.LoginKrakLikeIcon />
                     <S.AuthFormTitle component="condensedHeading5">Join the family</S.AuthFormTitle>
-                    <Formik initialValues={initialValues} validate={validate} onSubmit={onSubmit}>
+                    <Formik
+                        initialValues={SignupFormSchema.getDefault()}
+                        validationSchema={SignupFormSchema}
+                        onSubmit={onSubmit}
+                    >
                         {({ errors, isSubmitting, handleSubmit, isValid, dirty, touched }) => (
                             <form onSubmit={handleSubmit}>
                                 {/* Signup inputs */}
@@ -60,21 +90,24 @@ const Signup: NextPage = () => {
 
                                 {/* Submit */}
                                 <S.AuthSubmitContainer>
-                                    {(touched.username || touched.password) &&
-                                        (errors.password != null || errors.username != null) && (
-                                            <S.AuthSubmitErrorContainer>
-                                                {errors.username !== null && (
-                                                    <S.AuthSubmitError component="body2">
-                                                        {errors.username}
-                                                    </S.AuthSubmitError>
-                                                )}
-                                                {errors.password !== null && (
-                                                    <S.AuthSubmitError component="body2">
-                                                        {errors.password}
-                                                    </S.AuthSubmitError>
-                                                )}
-                                            </S.AuthSubmitErrorContainer>
-                                        )}
+                                    {/* First check if we have at least one error */}
+                                    {_.some(errors, (value) => !_.isNil(value)) && (
+                                        <S.AuthSubmitErrorContainer>
+                                            {/* Display the first error found which isn't nil and is touched */}
+                                            <S.AuthSubmitError component="body2">
+                                                {
+                                                    errors[
+                                                        _.first(
+                                                            Object.keys(errors).filter(
+                                                                (key) =>
+                                                                    !_.isNil(errors[key]) && !_.isNil(touched[key]),
+                                                            ),
+                                                        )
+                                                    ]
+                                                }
+                                            </S.AuthSubmitError>
+                                        </S.AuthSubmitErrorContainer>
+                                    )}
                                     <ButtonPrimary
                                         type="submit"
                                         loading={isSubmitting}
@@ -91,19 +124,6 @@ const Signup: NextPage = () => {
             </S.AuthPageContainer>
         </Layout>
     );
-};
-
-const validate = (values: SignupFormValues): FormikErrors<SignupFormValues> => {
-    const errors: FormikErrors<SignupFormValues> = {};
-
-    if (values.username === '' || values.password === '' || values.email === '') {
-        errors.username = 'All the fields are required';
-    }
-    if (values.password !== values.confirmPassword) {
-        errors.password = 'Passwords must match';
-    }
-
-    return errors;
 };
 
 export default Signup;
