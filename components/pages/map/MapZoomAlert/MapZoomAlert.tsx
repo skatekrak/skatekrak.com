08458ffe4cd0c80e94cfb5044ff4ecd2a3376ae4
Arthur Molinos <arthur.molinos@gmail.com>
2022-05-16 10:50:18 +0200

    lift: change MapZoomAlert visual

--- components/pages/map/MapZoomAlert/MapZoomAlert.tsx
+++ components/pages/map/MapZoomAlert/MapZoomAlert.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
-import Typography from 'components/Ui/typography/Typography';
 import Emoji from 'components/Ui/Icons/Emoji';
 import * as S from './MapZoomAlert.styled';
 
 const MapZoomAlert = () => {
     return (
         <S.MapZoomAlertContainer>
-            <S.MapZoomAlertMessageContainer>
-                <S.MapZoomAlertMessageEmojiContainer>
-                    <Emoji symbol="⚠️" label="warning" />
-                    &emsp;
-                    <Emoji symbol="📡" label="antenna" />
-                </S.MapZoomAlertMessageEmojiContainer>
-                <S.MapZoomAlertMessage component="body1">
-                    “Ground control to major tom, come back to earth ...”
-                </S.MapZoomAlertMessage>
-            </S.MapZoomAlertMessageContainer>
-            <S.MapZoomAlertActionContainer>
-                <Typography component="subtitle2">Zoom in to display spots</Typography>
-                <S.MapZoomAlertActionBackground />
-            </S.MapZoomAlertActionContainer>
+            <S.MapZoomAlertEmojiContainer>
+                <Emoji symbol="⚠️" label="warning" />
+                &emsp;
+                <Emoji symbol="📡" label="antenna" />
+            </S.MapZoomAlertEmojiContainer>
+            <S.MapZoomAlertMessage component="body1">
+                “Ground Control to Major Tom, come back to earth ...”
+            </S.MapZoomAlertMessage>
+            <S.MapZoomAlertAction component="body1">Zoom in to display spots</S.MapZoomAlertAction>
         </S.MapZoomAlertContainer>
     );
 };
