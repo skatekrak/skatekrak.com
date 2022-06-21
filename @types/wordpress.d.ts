feb6a327fb78fbefdfffb6e4f1b88b03d5388008
Maxime Cattet <maximecattet@gmx.com>
2022-06-21 14:29:50 +0200

    feat: post page are fully static

--- @types/wordpress.d.ts
+++ @types/wordpress.d.ts
@@ -9,21 +9,17 @@ declare module 'wordpress-types' {
     }
 
     export interface Post {
-        id?: number;
-        title?: { rendered?: string };
-        slug?: string;
-        link?: string;
-        date?: string;
-        date_gmt?: string;
-        modified_gmt?: string;
-        content?: { rendered?: string };
-        excerpt?: { rendered?: string };
-        featured_media?: number;
-        thumbnailImage?: string;
-        featuredImageFull?: string;
-        _format_video_embed?: string;
-        categories?: number[];
-        categoriesString?: string;
-        _embedded?: Record<string, any>;
+        id: number;
+        title: string;
+        date: string;
+        date_gmt: string;
+        slug: string;
+        content: string;
+        featuredImages: {
+            source_url: string;
+        }[];
+        video?: string;
+        tags: string[];
+        categories: string[];
     }
 }
--- components/pages/mag/Article/index.tsx
+++ components/pages/mag/Article/index.tsx
@@ -8,14 +8,12 @@ import createMarkup from 'lib/createMarkup';
 import decodeHTML from 'lib/decodeHTML';
 import createPropsGetter from 'lib/getProps';
 
-import Comment from 'components/pages/mag/Article/Comment';
 import ClipboardButton from 'components/Ui/Button/ClipboardButton';
 import { KrakLoading } from 'components/Ui/Icons/Spinners';
 import { Post } from 'wordpress-types';
 
 type Props = {
     post: Post;
-    sidebarNavIsOpen: boolean;
 } & Partial<DefaultProps>;
 
 type DefaultProps = Readonly<typeof defaultProps>;
@@ -27,7 +25,7 @@ const defaultProps = {
 const getProps = createPropsGetter(defaultProps);
 
 const Article = (rawProps: Props) => {
-    const { post, isLoading, sidebarNavIsOpen } = getProps(rawProps);
+    const { post, isLoading } = getProps(rawProps);
     if (isLoading) {
         return <KrakLoading />;
     }
@@ -35,7 +33,7 @@ const Article = (rawProps: Props) => {
     const baseURL = process.env.NEXT_PUBLIC_WEBSITE_URL;
 
     return (
-        <article id="mag-article" className={classNames({ hide: sidebarNavIsOpen })}>
+        <article id="mag-article">
             <Link href="/mag">
                 <a>
                     <img
@@ -53,13 +51,13 @@ const Article = (rawProps: Props) => {
                     <span id="mag-article-share-text">Share on:</span>
                     <FacebookShareButton
                         url={`${baseURL}/mag/${post.slug}`}
-                        quote={`${decodeHTML(post.title.rendered)} - shared via skatekrak.com`}
+                        quote={`${decodeHTML(post.title)} - shared via skatekrak.com`}
                     >
                         <FacebookIcon size={24} round />
                     </FacebookShareButton>
                     <TwitterShareButton
                         url={`${baseURL}/mag/${post.slug}`}
-                        title={decodeHTML(post.title.rendered)}
+                        title={decodeHTML(post.title)}
                         via="skatekrak"
                     >
                         <TwitterIcon size={24} round />
@@ -68,25 +66,21 @@ const Article = (rawProps: Props) => {
                 </div>
             </div>
             <header id="mag-article-header">
-                <p id="mag-article-category">{post.categoriesString}</p>
-                <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title.rendered)} />
-                <p id="mag-article-date">
-                    {format(parseISO(post.date), 'MMMM d')}, {format(parseISO(post.date), 'yyyy')}
-                </p>
+                <p id="mag-article-category">{post.categories.join(', ')}</p>
+                <h1 id="mag-article-title" dangerouslySetInnerHTML={createMarkup(post.title)} />
             </header>
             <main id="mag-article-main">
-                {post._format_video_embed ? (
-                    <div id="mag-article-video" dangerouslySetInnerHTML={createMarkup(post._format_video_embed)} />
+                {post.video ? (
+                    <div id="mag-article-video" dangerouslySetInnerHTML={createMarkup(post.video)} />
                 ) : (
                     <div id="mag-article-cover-img-container">
                         <div
                             id="mag-article-cover-img"
-                            style={{ backgroundImage: `url("${post.featuredImageFull}")` }}
+                            style={{ backgroundImage: `url("${post.featuredImages[0].source_url}")` }}
                         />
                     </div>
                 )}
-                <div id="mag-article-content" dangerouslySetInnerHTML={createMarkup(post.content.rendered)} />
-                <Comment />
+                <div id="mag-article-content" dangerouslySetInnerHTML={createMarkup(post.content)} />
             </main>
         </article>
     );
