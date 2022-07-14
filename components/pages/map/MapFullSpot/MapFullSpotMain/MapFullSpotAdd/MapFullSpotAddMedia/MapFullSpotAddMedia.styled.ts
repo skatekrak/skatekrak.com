1ef51ed5d9adf2019ae20fb28a18253ffaf0c595
Maxime Cattet <maximecattet@gmx.com>
2022-07-15 00:09:54 +0200

    chore: Remove unused import

--- components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotAdd/MapFullSpotAddMedia/MapFullSpotAddMedia.styled.ts
+++ components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotAdd/MapFullSpotAddMedia/MapFullSpotAddMedia.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Field, Form } from 'formik';
+import { Form } from 'formik';
 
 import Typography from 'components/Ui/typography/Typography';
 import media from 'styles/media';
