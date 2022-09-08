b143a0ad9d97b738ff77f701b7780f9bb302f47a
Maxime Cattet <maximecattet@gmx.com>
2022-09-08 09:40:07 +0200

    fix: Fix onspotpart

--- data/customMaps/_spots.ts
+++ data/customMaps/_spots.ts
@@ -158,11 +158,11 @@ Rediscover the first 2 games remastered in HD out now!
         id: 'onespotpart',
         name: 'One spot video-parts',
         subtitle: 'They own the spot!',
-        edit: 'Video-parts filmed intentionally at one spot.',
+        edito: 'Video-parts filmed intentionally at one spot.',
         about: `
-        From the good people at *Quartersnacks*, a map for all these local legends killing their plaza like no one else.
-        Here is the original article :
-        [https://quartersnacks.com/2021/07/introducing-the-quartersnacks-one-spot-part-map/](https://quartersnacks.com/2021/07/introducing-the-quartersnacks-one-spot-part-map/)
+From the good people at *Quartersnacks*, a map for all these local legends killing their plaza like no one else.
+Here is the original article :
+https://quartersnacks.com/2021/07/introducing-the-quartersnacks-one-spot-part-map
         `,
         spots: require('data/customMaps/onespotpart.json'),
         staging: true,
