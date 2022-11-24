818f2d6e82c8d8ab86ce9f6ad71fa78bb9b69202
Maxime Cattet <maximecattet@gmx.com>
2022-11-24 20:32:30 +0100

    feat: Upgrade maps

--- data/customMaps/_spots.ts
+++ data/customMaps/_spots.ts
@@ -10,6 +10,85 @@ We try to keep track of what's been done on these spots for a while now! Rewatch
         spots: require('data/customMaps/famous.json'),
     },
     {
+        id: 'webcam',
+        name: 'Webcams',
+        subtitle: 'Big Brother is watching you!',
+        edito: 'A hundred spots with 24/7 live webcams!',
+        about: `
+It all started with one live webcam in Brussels I used to check to see if the floor was dry downtown after it rained. It’s on a spot that is really crowdy all the time so you can actually see if it’s the right time to skate. I found out there was some other plazas, skateparks, or smaller spots worldwide with webcams. Imagine if you could film a part using them!
+        `,
+        spots: require('data/customMaps/webcam.json'),
+    },
+    {
+        id: 'andrew',
+        name: 'Andrew Reynolds Frontside Flips',
+        subtitle: 'The perfect flick!',
+        edito: 'The Boss signature trick!',
+        about: `
+Nobody can teach you frontside flips better than Andrew! It's been his signature trick for decades now and he brought it to quite a lot of iconic spots! 
+        `,
+        spots: require('data/customMaps/andrew.json'),
+    },
+    {
+        id: 'onespotpart',
+        name: 'One spot video-parts',
+        subtitle: 'They own the spot!',
+        edito: 'Video-parts filmed intentionally at one spot.',
+        about: `
+From the good people at *Quartersnacks*, a map for all these local legends killing their plaza like no one else.
+Here is the original article :
+https://quartersnacks.com/2021/07/introducing-the-quartersnacks-one-spot-part-map
+        `,
+        spots: require('data/customMaps/onespotpart.json'),
+        staging: false,
+        videos: ['https://www.youtube.com/playlist?list=PL2q6XHjX4Ujex44kmAFk0XL33MFCYSU57'],
+    },
+    {
+        id: 'betty',
+        name: 'Betty',
+        subtitle: 'Betty',
+        edito: 'Vans Europe Present: Betty',
+        about: `
+After last winter when the pandemic came around, Harry Billiet started filming his homies. Turned out they all killed it non-stop for a whole year so the project naturally became a real team video with the support of Vans. Most of the scenes are in Belgium instead of some in Luxemburg and Marseille.
+The video speaks for itself. These three Betties battled together through lockdowns.
+
+Skaters
+Jonathan Vlerick
+Jeroen Bruggeman
+Arthur Buyltinck
+Timothy Deconinck
+        `,
+        spots: require('data/customMaps/betty.json'),
+        staging: false,
+        videos: ['https://www.youtube.com/watch?v=vc1XPamfLUY'],
+    },
+    {
+        id: 'nycsubway',
+        name: 'NYC Subway Spots',
+        subtitle: '100% Illegal',
+        edito: 'Your guide to skating the NYC subway system.',
+        about: `
+Our friends at Jenkem cooked us a brief map to some of the most skateable spots within the NYC subway system a while ago. 
+
+It's a non-exhaustive list so feel free to add your spots on the map with the app!
+        
+As they reminded in their article, don't forget skateboarding on MTA property is 100% illegal so watch out before you go for a trick!
+        `,
+        spots: require('data/customMaps/nycsubway.json'),
+        videos: ['https://www.youtube.com/watch?v=uNkFTKsYqiY'],
+    },
+    {
+        id: 'googleearthers',
+        name: 'Google Earthers',
+        subtitle: "'Mankind's biggest tool/enemy'",
+        edito: "Jake Keenan's spots as seen in his Google Earthers episodes playing on jenkemmag.com",
+        about: `
+Jake Keenan uses Google Earth to find unusual and virgin skate spots in the NYC landscape. Google Earth and Street View are used by several skaters/filmers across the globe!
+        `,
+        spots: require('data/customMaps/googleearthers.json'),
+        videos: ['https://www.youtube.com/watch?v=JEd_YeihXnI&list=PLu4RiFW9tsSnAzCkMFvG0sfSzST642zgg'],
+    },
+    {
         id: 'history',
         name: 'Krak History Clip',
         subtitle: 'Krak History Clip',
@@ -73,40 +152,6 @@ If you want your event and all the details to be professionally executed by prov
         staging: true,
     },
     {
-        id: 'nycsubway',
-        name: 'NYC Subway Spots',
-        subtitle: '100% Illegal',
-        edito: 'Your guide to skating the NYC subway system.',
-        about: `
-Our friends at Jenkem cooked us a brief map to some of the most skateable spots within the NYC subway system a while ago. 
-
-It's a non-exhaustive list so feel free to add your spots on the map with the app!
-        
-As they reminded in their article, don't forget skateboarding on MTA property is 100% illegal so watch out before you go for a trick!
-        `,
-        spots: require('data/customMaps/nycsubway.json'),
-        videos: ['https://www.youtube.com/watch?v=uNkFTKsYqiY'],
-    },
-    {
-        id: 'betty',
-        name: 'Betty',
-        subtitle: 'Betty',
-        edito: 'Vans Europe Present: Betty',
-        about: `
-After last winter when the pandemic came around, Harry Billiet started filming his homies. Turned out they all killed it non-stop for a whole year so the project naturally became a real team video with the support of Vans. Most of the scenes are in Belgium instead of some in Luxemburg and Marseille.
-The video speaks for itself. These three Betties battled together through lockdowns.
-
-Skaters
-Jonathan Vlerick
-Jeroen Bruggeman
-Arthur Buyltinck
-Timothy Deconinck
-        `,
-        spots: require('data/customMaps/betty.json'),
-        staging: false,
-        videos: ['https://www.youtube.com/watch?v=vc1XPamfLUY'],
-    },
-    {
         id: 'knobbuster',
         name: 'The Knobbuster',
         subtitle: 'Who you gonna call?',
@@ -119,27 +164,7 @@ Let History continue! The show must go on!
         spots: require('data/customMaps/knobbuster.json'),
         staging: true,
     },
-    {
-        id: 'andrew',
-        name: 'Andrew Reynolds Frontside Flips',
-        subtitle: 'The perfect flick!',
-        edito: 'The Boss signature trick!',
-        about: `
-Nobody can teach you frontside flips better than Andrew! It's been his signature trick for decades now and he brought it to quite a lot of iconic spots! 
-        `,
-        spots: require('data/customMaps/andrew.json'),
-    },
-    {
-        id: 'googleearthers',
-        name: 'Google Earthers',
-        subtitle: "'Mankind's biggest tool/enemy'",
-        edito: "Jake Keenan's spots as seen in his Google Earthers episodes playing on jenkemmag.com",
-        about: `
-Jake Keenan uses Google Earth to find unusual and virgin skate spots in the NYC landscape. Google Earth and Street View are used by several skaters/filmers across the globe!
-        `,
-        spots: require('data/customMaps/googleearthers.json'),
-        videos: ['https://www.youtube.com/watch?v=JEd_YeihXnI&list=PLu4RiFW9tsSnAzCkMFvG0sfSzST642zgg'],
-    },
+    ,
     {
         id: 'thps',
         name: "Tony Hawk Pro Skater's Spots",
@@ -152,19 +177,5 @@ Rediscover the first 2 games remastered in HD out now!
         `,
         spots: require('data/customMaps/thps.json'),
         staging: true,
-    },
-    {
-        id: 'onespotpart',
-        name: 'One spot video-parts',
-        subtitle: 'They own the spot!',
-        edito: 'Video-parts filmed intentionally at one spot.',
-        about: `
-From the good people at *Quartersnacks*, a map for all these local legends killing their plaza like no one else.
-Here is the original article :
-https://quartersnacks.com/2021/07/introducing-the-quartersnacks-one-spot-part-map
-        `,
-        spots: require('data/customMaps/onespotpart.json'),
-        staging: false,
-        videos: ['https://www.youtube.com/playlist?list=PL2q6XHjX4Ujex44kmAFk0XL33MFCYSU57'],
     },
 ];
