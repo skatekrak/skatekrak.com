708b8c6c2910cc898e39d8416e9c25628b13f3c9
Maxime Cattet <maximecattet@gmx.com>
2023-02-08 12:02:48 +0100

    feat: Add query and hooks to fetch media

--- shared/feudartifice/hooks/media.ts
+++ shared/feudartifice/hooks/media.ts
@@ -1,6 +1,6 @@
 import Feudartifice from '..';
 import type { Media } from '../types';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 
 const { client } = Feudartifice;
 
@@ -11,6 +11,46 @@ const fetchMedia = async (id: string): Promise<Media> => {
 
 const useMedia = (id: string) => {
     return useQuery(['fetch-media', id], () => fetchMedia(id), {
+        keepPreviousData: true,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+    });
+};
+
+type BaseFetchMediasOptions = {
+    /// Default to 20
+    limit?: number;
+    hashtag?: string;
+};
+
+type FetchMediaOptions = BaseFetchMediasOptions & ({ older?: Date } | { newer?: Date });
+
+export const fetchMedias = async (options: FetchMediaOptions = {}) => {
+    const res = await client.get<Media[]>('/medias', {
+        params: options,
+    });
+    return res.data;
+};
+
+export const useMedias = (options: BaseFetchMediasOptions = {}) => {
+    return useQuery(['fetch-medias', options], () => fetchMedias(options), {
+        keepPreviousData: true,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+    });
+};
+
+export const useInfiniteMedias = (options: BaseFetchMediasOptions = {}) => {
+    return useInfiniteQuery(['fetch-medias', options], ({ pageParam }) => fetchMedias({ ...options, ...pageParam }), {
+        getNextPageParam: (lastPage) => {
+            const lastElement = lastPage[lastPage.length - 1];
+            if (lastElement != null) {
+                return { older: lastElement.createdAt };
+            }
+            return false;
+        },
         keepPreviousData: true,
         refetchOnMount: false,
         refetchOnReconnect: false,
