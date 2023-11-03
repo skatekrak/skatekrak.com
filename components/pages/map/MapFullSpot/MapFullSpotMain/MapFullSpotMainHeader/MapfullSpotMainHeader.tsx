4789499f41cd5ae0b7f0c2847f0315821927239e
Maxime Cattet <maximecattet@gmx.com>
2023-11-03 13:19:24 +0100

    fix: Fix missing import

--- components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotMainHeader/MapfullSpotMainHeader.tsx
+++ components/pages/map/MapFullSpot/MapFullSpotMain/MapFullSpotMainHeader/MapfullSpotMainHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 const MapfullSpotMainHeader = () => {
     const [isScrolled, setIsScrolled] = useState(false);
