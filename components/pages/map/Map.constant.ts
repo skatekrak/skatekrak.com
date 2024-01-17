4b82e1cd5d6a9bf288c2cecf58cacd7c8925cb78
Maxime Cattet <maximecattet@gmx.com>
2024-01-17 22:05:13 +0100

    feat: Update zooming

--- components/pages/map/Map.constant.ts
+++ components/pages/map/Map.constant.ts
@@ -1,4 +1,4 @@
-export const MAP_ZOOMS: [number, number, number, number] = [18, 12.5, 8.8, 2];
+export const MAP_ZOOMS: [number, number, number, number] = [18, 13, 7.5, 2];
 
 export const MAX_ZOOM_LEVEL = MAP_ZOOMS[0];
 export const ZOOM_DISPLAY_DOTS = MAP_ZOOMS[1];
