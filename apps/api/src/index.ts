b1a811f8905147934e4af7cead1a6b5ecd4e4429
Maxime Cattet <maximecattet@gmx.com>
2024-05-03 11:40:39 +0200

    fix: Fix CORS

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,6 @@ import { Elysia } from 'elysia';
 
 import { appRouter, createContext } from '@krak/trpc';
 
-const app = new Elysia().use(cors({ origin: /.*\.skatekrak\.com$/ })).use(trpc(appRouter, { createContext }));
+const app = new Elysia().use(cors({ origin: /.?*\.skatekrak\.com$/ })).use(trpc(appRouter, { createContext }));
 app.listen(3000);
 console.log('Server running on :3000');
