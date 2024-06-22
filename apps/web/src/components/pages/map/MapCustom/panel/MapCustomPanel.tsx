fad577f6f7228afd3753901e71b9bfcb2ae66676
Arthur Molinos <arthur.molinos@gmail.com>
2024-06-23 01:19:15 +0200

    feat: Ajoute simple scroll bar to custom map panel

--- apps/web/src/components/pages/map/MapCustom/panel/MapCustomPanel.tsx
+++ apps/web/src/components/pages/map/MapCustom/panel/MapCustomPanel.tsx
@@ -8,6 +8,7 @@ import Content from '@/components/pages/map/MapCustom/panel/Content';
 import { CustomMap } from '@/lib/map/types';
 import { Spot } from '@krak/carrelage-client';
 import MapCustomMediaCarousel from '@/components/pages/map/MapCustom/MapCustomMediaCarousel';
+import ScrollBar from '@/components/Ui/Scrollbar';
 
 export type MapCustomPanelTabs = 'media' | 'video' | 'spots';
 
@@ -55,81 +56,83 @@ const MapCustomPanel = ({ map, spots }: Props) => {
                     { 'bottom-auto': !isOpen },
                 )}
             >
-                {/** Navigation */}
-                <div className="flex justify-between py-4 px-6 border-b border-solid border-b-onDark-divider">
-                    <a className="group flex items-center gap-2 -ml-2 py-1 px-2 cursor-pointer" onClick={goBack}>
-                        <IconArrow className="w-5 shrink-0 fill-onDark-mediumEmphasis group-hover:fill-onDark-highEmphasis rotate-180" />
-                        <span className="whitespace-nowrap font-medium text-base group-hover:text-onDark-highEmphasis">
-                            Krak Map
-                        </span>
-                    </a>
-                    {isOpen ? (
-                        <button
-                            className="-mr-2 py-1 px-2 hover:text-onDark-highEmphasis"
-                            onClick={() => setIsOpen(false)}
-                        >
-                            Reduce details
-                        </button>
-                    ) : (
-                        <button className="flex items-center gap-3 text-base" onClick={() => setIsOpen(true)}>
-                            <NextImage
-                                className="rounded-full"
-                                src={`/images/map/custom-maps/${id}.png`}
-                                width={32}
-                                height={32}
-                                alt="custom map profile picture"
-                            />
-                            <span className="font-bold text-onDark-highEmphasis">{name}</span>
-                        </button>
-                    )}
-                </div>
-                {/** Map profile */}
-                {isOpen && (
-                    <>
-                        <div className="flex flex-col pt-4 px-6">
-                            <span className="font-medium capitalize">{categories[0]}</span>
-                            <NextImage
-                                className="mx-auto rounded-full"
-                                src={`/images/map/custom-maps/${id}.png`}
-                                width={120}
-                                height={120}
-                                alt="custom map profile picture"
-                                priority={true}
-                            />
-                            <h2 className="mt-4 font-bold text-2xl text-center text-onDark-highEmphasis">{name}</h2>
-                            {about && (
-                                <>
-                                    <p
-                                        id="map-custom-panel-about"
-                                        className={classNames('mt-4 text-sm text-center', {
-                                            'line-clamp-4': !isReadMoreOpen,
-                                        })}
-                                    >
-                                        {about}
-                                    </p>
-                                    {showReadMore && (
-                                        <button
-                                            className="mt-1 mx-auto p-1 text-sm underline"
-                                            onClick={() => setIsReadMoreOpen(!isReadMoreOpen)}
+                <ScrollBar maxHeight="100%">
+                    {/** Navigation */}
+                    <div className="flex justify-between py-4 px-6 border-b border-solid border-b-onDark-divider">
+                        <a className="group flex items-center gap-2 -ml-2 py-1 px-2 cursor-pointer" onClick={goBack}>
+                            <IconArrow className="w-5 shrink-0 fill-onDark-mediumEmphasis group-hover:fill-onDark-highEmphasis rotate-180" />
+                            <span className="whitespace-nowrap font-medium text-base group-hover:text-onDark-highEmphasis">
+                                Krak Map
+                            </span>
+                        </a>
+                        {isOpen ? (
+                            <button
+                                className="-mr-2 py-1 px-2 hover:text-onDark-highEmphasis"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Reduce details
+                            </button>
+                        ) : (
+                            <button className="flex items-center gap-3 text-base" onClick={() => setIsOpen(true)}>
+                                <NextImage
+                                    className="rounded-full"
+                                    src={`/images/map/custom-maps/${id}.png`}
+                                    width={32}
+                                    height={32}
+                                    alt="custom map profile picture"
+                                />
+                                <span className="font-bold text-onDark-highEmphasis">{name}</span>
+                            </button>
+                        )}
+                    </div>
+                    {/** Map profile */}
+                    {isOpen && (
+                        <>
+                            <div className="flex flex-col pt-4 px-6">
+                                <span className="font-medium capitalize">{categories[0]}</span>
+                                <NextImage
+                                    className="mx-auto rounded-full"
+                                    src={`/images/map/custom-maps/${id}.png`}
+                                    width={120}
+                                    height={120}
+                                    alt="custom map profile picture"
+                                    priority={true}
+                                />
+                                <h2 className="mt-4 font-bold text-2xl text-center text-onDark-highEmphasis">{name}</h2>
+                                {about && (
+                                    <>
+                                        <p
+                                            id="map-custom-panel-about"
+                                            className={classNames('mt-4 text-sm text-center', {
+                                                'line-clamp-4': !isReadMoreOpen,
+                                            })}
                                         >
-                                            {isReadMoreOpen ? 'Hide' : 'Read more'}
-                                        </button>
-                                    )}
-                                </>
-                            )}
-                        </div>
-                        {/** Tabs */}
-                        <div className="flex gap-6 justify-center flex-wrap my-8 px-6">
-                            <Tab title="media" onClick={() => setOpenTab('media')} isActive={openTab === 'media'} />
-                            <Tab title="video" onClick={() => setOpenTab('video')} isActive={openTab === 'video'} />
-                            <Tab title="spots" onClick={() => setOpenTab('spots')} isActive={openTab === 'spots'} />
-                        </div>
-                        {/** Content */}
-                        <div className="grow flex flex-col gap-6 px-6 pb-8">
-                            <Content map={map} activeTab={openTab} spots={spots} />
-                        </div>
-                    </>
-                )}
+                                            {about}
+                                        </p>
+                                        {showReadMore && (
+                                            <button
+                                                className="mt-1 mx-auto p-1 text-sm underline"
+                                                onClick={() => setIsReadMoreOpen(!isReadMoreOpen)}
+                                            >
+                                                {isReadMoreOpen ? 'Hide' : 'Read more'}
+                                            </button>
+                                        )}
+                                    </>
+                                )}
+                            </div>
+                            {/** Tabs */}
+                            <div className="flex gap-6 justify-center flex-wrap my-8 px-6">
+                                <Tab title="media" onClick={() => setOpenTab('media')} isActive={openTab === 'media'} />
+                                <Tab title="video" onClick={() => setOpenTab('video')} isActive={openTab === 'video'} />
+                                <Tab title="spots" onClick={() => setOpenTab('spots')} isActive={openTab === 'spots'} />
+                            </div>
+                            {/** Content */}
+                            <div className="grow flex flex-col gap-6 px-6 pb-8">
+                                <Content map={map} activeTab={openTab} spots={spots} />
+                            </div>
+                        </>
+                    )}
+                </ScrollBar>
             </div>
         </>
     );
