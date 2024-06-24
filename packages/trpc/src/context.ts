fc3aee23cee963fc9b1ea6e60417c07b6c8325f6
Maxime Cattet <maximecattet@gmx.com>
2024-06-24 14:45:34 +0200

    chore: Add check for mongodb uri

--- packages/trpc/src/context.ts
+++ packages/trpc/src/context.ts
@@ -1,6 +1,10 @@
 import { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import { MongoClient } from 'mongodb';
 
+if (process.env.MONGODB_URI === undefined) {
+    throw new Error('MONGODB_URI is not defined');
+}
+
 const client = new MongoClient(process.env.MONGODB_URI!);
 client.connect();
 
