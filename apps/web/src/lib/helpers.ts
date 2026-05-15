2e7f65360672a940379ab93b89876e3a6750f6fb
Maxime Cattet <m@root.bar>
2026-05-16 00:22:28 +0200

    refactor(helpers.ts): add generic type parameters to removeEmptyStringAndNull function for better type safety and inference

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -2,9 +2,9 @@ export function flatten<T>(array: T[][]): T[] {
     return (array ?? []).reduce((acc, val) => acc.concat(val), []);
 }
 
-export function removeEmptyStringAndNull(object: any) {
-    const keys = Object.keys(object);
-    return keys.reduce((newItem, key) => {
+export function removeEmptyStringAndNull<T extends Record<string, unknown>>(object: T): Partial<T> {
+    const keys = Object.keys(object) as (keyof T)[];
+    return keys.reduce<Partial<T>>((newItem, key) => {
         if (object[key] != null && object[key] !== '') {
             newItem[key] = object[key];
         }
